a92a274a29bd3bbfc6d2ae33608a9680f4b717f8
OmniBlade <nadia_xy@hotmail.com>
2017-04-11 09:12:18 +0100

    fixed wrong project header on chukio.cpp

--- src/w3d/lib/chunkio.cpp
+++ src/w3d/lib/chunkio.cpp
@@ -1,10 +1,10 @@
 ////////////////////////////////////////////////////////////////////////////////
-///                       --  C H R O N O  L I B --                          ///
+//                               --  THYME  --                                //
 ////////////////////////////////////////////////////////////////////////////////
 //
-//  Project Name:: ChronoLib
+//  Project Name:: Thyme
 //
-//          File:: CHUNKIO.CPP
+//          File:: CHUNKIO.H
 //
 //        Author:: OmniBlade
 //
@@ -13,13 +13,13 @@
 //   Description:: Classes for reading and writing the binary chunk format used
 //                 in WW3D and SAGE games.
 //
-//       License:: ChronoLib is free software: you can redistribute it and/or 
+//       License:: Thyme is free software: you can redistribute it and/or 
 //                 modify it under the terms of the GNU General Public License 
 //                 as published by the Free Software Foundation, either version 
 //                 2 of the License, or (at your option) any later version.
 //
 //                 A full copy of the GNU General Public License can be found in
-//                 LICENSE.md
+//                 LICENSE
 //
 ////////////////////////////////////////////////////////////////////////////////
 #include    "chunkio.h"

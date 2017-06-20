0d3e795ae60771daedfc01b1c006d40ddae62b5b
OmniBlade <nadia_xy@hotmail.com>
2017-06-20 22:00:21 +0100

    Initial work on MultiplayerSettings class.

---
+++ src/game/common/multiplayersettings.cpp
@@ -0,0 +1,100 @@
+////////////////////////////////////////////////////////////////////////////////
+//                               --  THYME  --                                //
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Project Name:: Thyme
+//
+//          File:: MULTIPLAYERSETTINGS.CPP
+//
+//        Author:: OmniBlade
+//
+//  Contributors:: 
+//
+//   Description:: Multiplayer setting handling class.
+//
+//       License:: Thyme is free software: you can redistribute it and/or 
+//                 modify it under the terms of the GNU General Public License 
+//                 as published by the Free Software Foundation, either version 
+//                 2 of the License, or (at your option) any later version.
+//
+//                 A full copy of the GNU General Public License can be found in
+//                 LICENSE
+//
+////////////////////////////////////////////////////////////////////////////////
+#include "multiplayersettings.h"
+#include "gamemath.h"
+
+using GameMath::Floor;
+
+#ifdef THYME_STANDALONE
+MultiplayerSettings *g_theMultiplayerSettings = nullptr;
+#endif
+
+FieldParse MultiplayerColorDefinition::s_colorFieldParsetable[] = {
+    { "TooltipName", &INI::Parse_AsciiString, nullptr, offsetof(MultiplayerColorDefinition, m_tooltipName) },
+    { "RGBColor", &INI::Parse_RGB_Color, nullptr, offsetof(MultiplayerColorDefinition, m_rgbValue) },
+    { "RGBNightColor", &INI::Parse_RGB_Color, nullptr, offsetof(MultiplayerColorDefinition, m_rgbNightValue) },
+    { nullptr, nullptr, nullptr, 0 }
+};
+
+void MultiplayerColorDefinition::Set_Color(RGBColor rgb)
+{
+    m_color =
+        (int)Floor(rgb.blue * 255.0f) |
+        ((int)Floor(rgb.green * 255.0f) << 8) |
+        ((int)Floor(rgb.red * 255.0f) << 16) |
+        0xFF000000;
+}
+
+void MultiplayerColorDefinition::Set_Night_Color(RGBColor rgb)
+{
+    m_nightColor =
+        (int)Floor(rgb.blue * 255.0f) |
+        ((int)Floor(rgb.green * 255.0f) << 8) |
+        ((int)Floor(rgb.red * 255.0f) << 16) |
+        0xFF000000;
+}
+
+void MultiplayerColorDefinition::Parse_Color_Definition(INI *ini)
+{
+    AsciiString token = ini->Get_Next_Token();
+    MultiplayerColorDefinition *def = g_theMultiplayerSettings->Find_Color_Definition(token);
+    
+    if ( def == nullptr ) {
+        def = g_theMultiplayerSettings->New_Color_Definition(token);
+    }
+
+    ini->Init_From_INI(def, s_colorFieldParsetable);
+    def->Set_Color(def->m_rgbValue);
+    def->Set_Night_Color(def->m_rgbNightValue);
+}
+
+MultiplayerSettings::MultiplayerSettings() :
+    m_initialCreditsMin(0),
+    m_initialCreditsMax(0),
+    m_startCountdownTimer(0),
+    m_maxBeaconsPerPlayer(0),
+    m_useShroud(true),
+    m_showRandomPlayerTemplate(true),
+    m_showRandomStartPos(true),
+    m_showRandomColor(true),
+    m_colorList(),
+    m_numColors(0),
+    m_colorDef1(),
+    m_colorDef2(),
+    m_moneyVec(),
+    m_startingMoney(),
+    m_moneyDefault(false)
+{
+
+}
+
+MultiplayerColorDefinition *MultiplayerSettings::Find_Color_Definition(AsciiString name)
+{
+    return nullptr;
+}
+
+MultiplayerColorDefinition *MultiplayerSettings::New_Color_Definition(AsciiString name)
+{
+    return nullptr;
+}
---
+++ src/game/common/multiplayersettings.h
@@ -0,0 +1,119 @@
+////////////////////////////////////////////////////////////////////////////////
+//                               --  THYME  --                                //
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Project Name:: Thyme
+//
+//          File:: MULTIPLAYERSETTINGS.H
+//
+//        Author:: OmniBlade
+//
+//  Contributors:: 
+//
+//   Description:: Multiplayer setting handling class.
+//
+//       License:: Thyme is free software: you can redistribute it and/or 
+//                 modify it under the terms of the GNU General Public License 
+//                 as published by the Free Software Foundation, either version 
+//                 2 of the License, or (at your option) any later version.
+//
+//                 A full copy of the GNU General Public License can be found in
+//                 LICENSE
+//
+////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#ifndef MULTIPLAYERSETTINGS_H
+#define MULTIPLAYERSETTINGS_H
+
+#ifndef THYME_STANDALONE
+#include "hooker.h"
+#endif
+
+#include "asciistring.h"
+#include "color.h"
+#include "ini.h"
+#include "money.h"
+#include "subsysteminterface.h"
+#include <map>
+#include <vector>
+
+class MultiplayerColorDefinition
+{
+public:
+    MultiplayerColorDefinition();
+    MultiplayerColorDefinition &operator=(MultiplayerColorDefinition &rval);
+    void Set_Color(RGBColor rgb);
+    void Set_Night_Color(RGBColor rgb);
+
+    static void Parse_Color_Definition(INI *ini);
+private:
+    AsciiString m_tooltipName;
+    RGBColor m_rgbValue;
+    int m_color;
+    RGBColor m_rgbNightValue;
+    int m_nightColor;
+
+    static FieldParse s_colorFieldParsetable[];
+};
+
+inline MultiplayerColorDefinition::MultiplayerColorDefinition() :
+    m_tooltipName(),
+    m_rgbValue{ 1.0f, 1.0f, 1.0f },
+    m_color(-1),
+    m_rgbNightValue{ 1.0f, 1.0f, 1.0f },
+    m_nightColor(-1)
+{
+
+}
+
+inline MultiplayerColorDefinition &MultiplayerColorDefinition::operator=(MultiplayerColorDefinition &rval)
+{
+    if ( this != &rval ) {
+        m_tooltipName = rval.m_tooltipName;
+        m_rgbValue = rval.m_rgbValue;
+        m_color = rval.m_color;
+        m_rgbNightValue = rval.m_rgbNightValue;
+        m_nightColor = rval.m_nightColor;
+    }
+
+    return *this;
+}
+
+class MultiplayerSettings : public SubsystemInterface
+{
+public:
+    MultiplayerSettings();
+
+    // Subsystem interface implementation.
+    virtual void Init() override {}
+    virtual void Reset() override {}
+    virtual void Update() override {}
+
+    MultiplayerColorDefinition *Find_Color_Definition(AsciiString name);
+    MultiplayerColorDefinition *New_Color_Definition(AsciiString name);
+private:
+    int m_initialCreditsMin;
+    int m_initialCreditsMax;
+    int m_startCountdownTimer;
+    int m_maxBeaconsPerPlayer;
+    bool m_useShroud;
+    bool m_showRandomPlayerTemplate;
+    bool m_showRandomStartPos;
+    bool m_showRandomColor;
+    std::map<AsciiString, MultiplayerColorDefinition> m_colorList;
+    int m_numColors;
+    MultiplayerColorDefinition m_colorDef1;
+    MultiplayerColorDefinition m_colorDef2;
+    std::vector<Money> m_moneyVec;
+    Money m_startingMoney;
+    bool m_moneyDefault;
+};
+
+#ifdef THYME_STANDALONE
+extern MultiplayerSettings *g_theMultiplayerSettings;
+#else
+#define g_theMultiplayerSettings (Make_Global<MultiplayerSettings*>(0x00A2BE58))
+#endif
+
+#endif // MULTIPLAYERSETTINGS_H

8fef1e0b9aab8446299dbf57e6ba78bc2eaf3496
OmniBlade <nadia_xy@hotmail.com>
2017-09-06 13:32:47 +0100

    Implements FunctionLexicon base class.

---
+++ src/game/common/system/functionlexicon.cpp
@@ -0,0 +1,156 @@
+/**
+ * @file
+ *
+ * @Author OmniBlade
+ *
+ * @brief Interface for interface function pointer manager.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "functionlexicon.h"
+
+FunctionLexicon::FunctionLexicon()
+{
+    memset(m_tables, 0, sizeof(m_tables));
+}
+
+/**
+ * @brief Load a table of function pointers, generating keys for their names for easy lookup.
+ */
+void FunctionLexicon::Load_Table(TableEntry *table, TableIndex index)
+{
+    if (table == nullptr) {
+        return;
+    }
+
+    // Generate keys for all the entry names.
+    for (TableEntry *te = table; te->name != nullptr; ++te) {
+        te->key = g_theNameKeyGenerator->Name_To_Key(te->name);
+    }
+
+    m_tables[index] = table;
+}
+
+/**
+ * @brief Internal function that validates that no duplicate functions exist in the lexicon.
+ */
+bool FunctionLexicon::Validate()
+{
+    bool valid = true;
+
+    // Iterate over all the tables looking for function duplicates, log if found and fail validate
+    for (int i = 0; i < MAX_FUNCTION_TABLES; ++i) {
+        for (TableEntry *i_ent = m_tables[i]; i_ent != nullptr && i_ent->key != NAMEKEY_INVALID; ++i_ent) {
+            for (int j = 0; j < MAX_FUNCTION_TABLES; ++j) {
+                for (TableEntry *j_ent = m_tables[j]; j_ent != nullptr && j_ent->key != NAMEKEY_INVALID; ++i_ent) {
+                    if (i_ent != j_ent && i_ent->func == j_ent->func) {
+                        DEBUG_LOG("WARNING! Function lexicon entries match same address! '%s' and '%s'\n",
+                            i_ent->name,
+                            j_ent->name);
+                        valid = false;
+                    }
+                }
+            }
+        }
+    }
+
+    return valid;
+}
+
+/**
+ * @brief Internal function to look up a function in the lexicon from a key.
+ */
+void *FunctionLexicon::Key_To_Func(NameKeyType key, TableEntry *table)
+{
+    if (key == NAMEKEY_INVALID || table == nullptr) {
+        return nullptr;
+    }
+
+    for (TableEntry *tbl = table; tbl != nullptr && tbl->key != NAMEKEY_INVALID; ++tbl) {
+        if (tbl->key == key) {
+            return tbl->func;
+        }
+    }
+
+    return nullptr;
+}
+
+/**
+ * @brief Initialises all the tables with appropriate key values.
+ */
+void FunctionLexicon::Init()
+{
+#ifndef THYME_STANDALONE
+    Call_Method<void, FunctionLexicon>(0x004F3EC0, this);
+#else
+// TODO requires several function pointer tables, do them as functions that call them require it?
+#endif
+}
+
+/**
+ * @brief Finds a function from a key in the table specified by index.
+ */
+void *FunctionLexicon::Find_Function(NameKeyType key, TableIndex index)
+{
+    if (key == NAMEKEY_INVALID) {
+        return nullptr;
+    }
+
+    // If the index is specific, access that table.
+    if (index != TABLE_ANY) {
+        return Key_To_Func(key, m_tables[index]);
+    }
+
+    // Otherwise iterate through all tables to find a match.
+    for (int i = 0; i < MAX_FUNCTION_TABLES; ++i) {
+        void *tmp = Key_To_Func(key, m_tables[i]);
+
+        if (tmp != nullptr) {
+            return tmp;
+        }
+    }
+
+    return nullptr;
+}
+
+/**
+ * @brief Specifically locates functions relating to window drawing.
+ */
+drawfunc_t FunctionLexicon::Game_Win_Draw_Func(NameKeyType key, TableIndex index)
+{
+    if (index != TABLE_ANY) {
+        return (drawfunc_t)Find_Function(key, index);
+    }
+
+    void *tmp = Find_Function(key, TABLE_GAME_WIN_DEVICEDRAW);
+
+    if (tmp == nullptr) {
+        tmp = Find_Function(key, TABLE_GAME_WIN_DRAW);
+    }
+
+    return (drawfunc_t)tmp;
+}
+
+/**
+ * @brief Specifically locates functions relating to layout initialisation.
+ */
+layoutfunc_t FunctionLexicon::Win_Layout_Init_Func(NameKeyType key, TableIndex index)
+{
+    if (index != TABLE_ANY) {
+        return (layoutfunc_t)Find_Function(key, index);
+    }
+
+    void *tmp = Find_Function(key, TABLE_WIN_LAYOUT_INIT);
+
+    if (tmp == nullptr) {
+        tmp = Find_Function(key, TABLE_LAYOUT_INIT);
+    }
+
+    return (layoutfunc_t)tmp;
+}
---
+++ src/game/common/system/functionlexicon.h
@@ -0,0 +1,91 @@
+/**
+ * @file
+ *
+ * @Author OmniBlade
+ *
+ * @brief Interface for interface function pointer manager.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef FUNCTIONLEXICON_H
+#define FUNCTIONLEXICON_H
+
+#include "namekeygenerator.h"
+#include "subsysteminterface.h"
+
+#ifndef THYME_STANDALONE
+#include "hooker.h"
+#endif
+
+class GameWindow;
+class WinInstanceData;
+class WinLayout;
+
+typedef void (*drawfunc_t)(GameWindow *, WinInstanceData *);
+typedef void (*layoutfunc_t)(WinLayout *, void *);
+
+class FunctionLexicon : public SubsystemInterface
+{
+public:
+    enum TableIndex
+    {
+        TABLE_ANY = -1,
+        TABLE_GAME_WIN_SYSTEM,
+        TABLE_GAME_WIN_INPUT,
+        TABLE_GAME_WIN_TOOLTIP,
+        TABLE_GAME_WIN_DEVICEDRAW,
+        TABLE_GAME_WIN_DRAW,
+        TABLE_LAYOUT_INIT,
+        TABLE_WIN_LAYOUT_INIT,
+        TABLE_WIN_LAYOUT_UPDATE,
+        TABLE_WIN_LAYOUT_SHUTDOWN,
+        MAX_FUNCTION_TABLES,
+    };
+
+    struct TableEntry
+    {
+        NameKeyType key;
+        const char *name;
+        void *func;
+    };
+
+public:
+    FunctionLexicon();
+    virtual ~FunctionLexicon() {}
+
+    virtual void Init() override;
+    virtual void Reset() override { Init(); }
+    virtual void Update() override {}
+
+    void *Find_Function(NameKeyType key, TableIndex index);
+    drawfunc_t Game_Win_Draw_Func(NameKeyType key, TableIndex index);
+    layoutfunc_t Win_Layout_Init_Func(NameKeyType key, TableIndex index);
+
+#ifndef THYME_STANDALONE
+    static void Hook_Me();
+#endif
+protected:
+    void Load_Table(TableEntry *table, TableIndex index);
+    bool Validate();
+    void *Key_To_Func(NameKeyType key, TableEntry *table);
+
+private:
+    TableEntry *m_tables[MAX_FUNCTION_TABLES];
+};
+
+#ifndef THYME_STANDALONE
+inline void FunctionLexicon::Hook_Me()
+{
+    Hook_Method(Make_Method_Ptr<void, FunctionLexicon, TableEntry*, TableIndex>(0x004F3D00), &Load_Table);
+}
+#endif
+
+#endif // FUNCTIONLEXICON_H

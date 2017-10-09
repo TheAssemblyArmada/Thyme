0158dd65c021c52120fb3e008ea382df6e8f62b5
OmniBlade <nadia_xy@hotmail.com>
2017-10-09 12:34:52 +0100

    Implements data stream classes and interfaces.

---
+++ src/game/common/system/inputstream.h
@@ -0,0 +1,28 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Stream interface providing a readable data stream.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef INPUTSTREAM_H
+#define INPUTSTREAM_H
+
+#include "always.h"
+
+class InputStream
+{
+public:
+    virtual int Read(void *dst, int size) = 0;
+};
+
+#endif

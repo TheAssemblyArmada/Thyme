1c4ea6760a1d10d2db5e6d4d17430c57c2066347
OmniBlade <nadia_xy@hotmail.com>
2017-10-24 21:40:00 +0100

    Implements DataChunkTableOfContents.

---
+++ src/game/common/system/datachunktoc.h
@@ -0,0 +1,66 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Class for handling the binary chunk format table of contents used by SAGE engine maps.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef DATACHUNKTOC_H
+#define DATACHUNKTOC_H
+
+#include "always.h"
+#include "asciistring.h"
+#include "chunkinputstream.h"
+#include "mempoolobj.h"
+#include "outputstream.h"
+
+class DataChunkTableOfContents
+{
+    class Mapping : public MemoryPoolObject
+    {
+        IMPLEMENT_POOL(Mapping);
+        friend class DataChunkTableOfContents;
+
+    public:
+        virtual ~Mapping() {}
+
+    private:
+        Mapping() : m_next(nullptr), m_name(), m_id(0) {}
+
+    private:
+        Mapping *m_next;
+        AsciiString m_name;
+        unsigned m_id;
+    };
+
+public:
+    DataChunkTableOfContents() : m_list(nullptr), m_listLength(0), m_nextID(1), m_headerOpened(false) {}
+    ~DataChunkTableOfContents();
+
+    unsigned Get_ID(const AsciiString &name);
+    AsciiString Get_Name(unsigned id);
+    unsigned Allocate_ID(const AsciiString &name);
+    void Read(ChunkInputStream &stream);
+    void Write(OutputStream &stream);
+    bool Header_Opened() { return m_headerOpened; }
+
+private:
+    Mapping *Find_Mapping(const AsciiString &name);
+
+private:
+    Mapping *m_list;
+    int m_listLength;
+    unsigned m_nextID;
+    bool m_headerOpened;
+};
+
+#endif // DATACHUNKTOC_H

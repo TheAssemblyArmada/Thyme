b1dbfd13e016305a46439626ffc073224a7b119e
OmniBlade <nadia_xy@hotmail.com>
2017-11-16 22:42:36 +0000

    Implements a skeleton class layout for Script.

---
+++ src/game/logic/scriptengine/script.cpp
@@ -0,0 +1,68 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Class representing a script object.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "script.h"
+#include "xfer.h"
+#include "scriptaction.h"
+#include "scriptcondition.h"
+
+Script::Script() :
+    m_scriptName(),
+    m_comment(),
+    m_conditionComment(),
+    m_actionComment(),
+    m_unkInt1(0),
+    m_isActive(true),
+    m_isOneShot(true),
+    m_isSubroutine(false),
+    m_easy(true),
+    m_normal(true),
+    m_hard(true),
+    m_condition(nullptr),
+    m_action(nullptr),
+    m_actionFalse(nullptr),
+    m_nextScript(nullptr),
+    m_unkInt2(0),
+    m_hasWarnings(false),
+    m_conditionTeamName(),
+    m_unkInt3(0),
+    m_unkInt4(0),
+    m_unkInt5(0)
+{
+
+}
+
+Script::~Script()
+{
+    Script *saved;
+    for (Script *next = m_nextScript; next != nullptr; next = saved) {
+        saved = next->m_nextScript;
+        next->m_nextScript = nullptr;
+        delete next;
+        next = saved;
+    }
+
+    delete m_condition;
+    delete m_action;
+    delete m_actionFalse;
+}
+
+void Script::Xfer_Snapshot(Xfer *xfer)
+{
+    uint8_t version = 1;
+    xfer->xferVersion(&version, 1);
+    bool active = m_isActive;
+    xfer->xferBool(&active);
+    m_isActive = active;
+}
---
+++ src/game/logic/scriptengine/script.h
@@ -0,0 +1,64 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Class representing a script object.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef SCRIPT_H
+#define SCRIPT_H
+
+#include "always.h"
+#include "asciistring.h"
+#include "mempoolobj.h"
+#include "snapshot.h"
+
+class OrCondition;
+class ScriptAction;
+
+class Script : public MemoryPoolObject, public SnapShot
+{
+    IMPLEMENT_POOL(Script);
+public:
+    Script();
+    virtual ~Script();
+
+    // Snapshot interface methods.
+    virtual void CRC_Snapshot(Xfer *xfer) override {}
+    virtual void Xfer_Snapshot(Xfer *xfer) override;
+    virtual void Load_Post_Process() override {}
+
+private:
+    AsciiString m_scriptName;
+    AsciiString m_comment;
+    AsciiString m_conditionComment;
+    AsciiString m_actionComment;
+    int m_unkInt1;
+    bool m_isActive;
+    bool m_isOneShot;
+    bool m_isSubroutine;
+    bool m_easy;
+    bool m_normal;
+    bool m_hard;
+    OrCondition *m_condition;
+    ScriptAction *m_action;
+    ScriptAction *m_actionFalse;
+    Script *m_nextScript;
+    int m_unkInt2;
+    bool m_hasWarnings;
+    AsciiString m_conditionTeamName;
+    int m_unkInt3;
+    int m_unkInt4;
+    int m_unkInt5;
+};
+
+#endif // SCRIPT_H

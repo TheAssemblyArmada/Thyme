3b26f60ba2fa1b85637b225741f55bd634563347
OmniBlade <nadia_xy@hotmail.com>
2018-01-15 08:30:12 +0000

    Implements assignment operators from multibyte to UnicodeString.

--- src/game/common/system/unicodestring.h
+++ src/game/common/system/unicodestring.h
@@ -76,6 +76,19 @@ public:
         Set(string);
         return *this;
     }
+
+    UnicodeString &operator=(AsciiString const &string)
+    {
+        Translate(string);
+        return *this;
+    }
+
+    UnicodeString &operator=(const char *s)
+    {
+        Translate(s);
+        return *this;
+    }
+
     // UnicodeString &operator=(AsciiString const &string) { Set(string); return *this; }
 
     UnicodeString &operator+=(char16_t s)

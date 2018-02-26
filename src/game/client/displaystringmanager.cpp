93567907c4cf8fae81d171918902aecfa2c268d3
OmniBlade <nadia_xy@hotmail.com>
2018-02-26 23:09:27 +0000

    Implements DisplayStringManager.

---
+++ src/game/client/displaystringmanager.cpp
@@ -0,0 +1,59 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Virtual base class for managing DisplayString based classes.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "displaystringmanager.h"
+
+#ifndef THYME_STANDALONE
+DisplayStringManager *&g_theDisplayStringManger = Make_Global<DisplayStringManager *>(0x00A2A6C4);
+#else
+DisplayStringManager *g_theDisplayStringManger = nullptr;
+#endif
+
+/**
+ * @brief Links a DisplayString into the managers internal list.
+ *
+ * 0x0041FE10
+ */
+void DisplayStringManager::Link(DisplayString *string)
+{
+    string->m_next = m_stringList;
+
+    if (m_stringList != nullptr) {
+        m_stringList->m_prev = string;
+    }
+
+    m_stringList = string;
+}
+
+/**
+ * @brief Unlinks a DisplayString from the managers internal list.
+ *
+ * 0x0041FE30
+ */
+void DisplayStringManager::Unlink(DisplayString *string)
+{
+    DisplayString *next = string->m_next;
+
+    if (next != nullptr) {
+        next->m_prev = string->m_prev;
+    }
+
+    DisplayString *prev = string->m_prev;
+
+    if (prev != nullptr) {
+        prev->m_next = string->m_next;
+    } else {
+        m_stringList = string->m_next;
+    }
+}
---
+++ src/game/client/displaystringmanager.h
@@ -0,0 +1,55 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Virtual base class for managing DisplayString based classes.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef DISPLAYSTRINGMANAGER_H
+#define DISPLAYSTRINGMANAGER_H
+
+#include "always.h"
+#include "displaystring.h"
+#include "subsysteminterface.h"
+
+class DisplayStringManager : public SubsystemInterface
+{
+public:
+    DisplayStringManager();
+    virtual ~DisplayStringManager();
+
+    virtual void Init() override {}
+    virtual void Reset() override {}
+    virtual void Update() override {}
+    virtual DisplayString *New_Display_String() = 0;
+    virtual void Free_Display_String(DisplayString *string) = 0;
+    virtual DisplayString *Get_Group_Numeral_String(int num) = 0;
+    virtual DisplayString *Get_Formation_Letter_String() = 0;
+
+protected:
+    void Link(DisplayString *string);
+    void Unlink(DisplayString *string);
+
+private:
+    DisplayString *m_stringList;
+    DisplayString *m_currentCheckpoint;
+};
+
+#ifndef THYME_STANDALONE
+#include "hooker.h"
+
+extern DisplayStringManager *&g_theDisplayStringManger;
+#else
+extern DisplayStringManager *g_theDisplayStringManger;
+#endif
+
+#endif // DISPLAYSTRINGMANAGER_H

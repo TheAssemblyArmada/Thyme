a3c73ecd22c98c3759a6260ebf9d0b7687a462d4
OmniBlade <nadia_xy@hotmail.com>
2018-03-06 08:22:26 +0000

    Applies formatting to systimer.

--- src/w3d/lib/systimer.cpp
+++ src/w3d/lib/systimer.cpp
@@ -1,31 +1,22 @@
-////////////////////////////////////////////////////////////////////////////////
-//                               --  THYME  --                                //
-////////////////////////////////////////////////////////////////////////////////
-//
-//  Project Name:: Thyme
-//
-//          File:: SYSTIMER.CPP
-//
-//        Author:: OmniBlade
-//
-//  Contributors:: 
-//
-//   Description:: Tracks the system time.
-//
-//       License:: Thyme is free software: you can redistribute it and/or 
-//                 modify it under the terms of the GNU General Public License 
-//                 as published by the Free Software Foundation, either version 
-//                 2 of the License, or (at your option) any later version.
-//
-//                 A full copy of the GNU General Public License can be found in
-//                 LICENSE
-//
-////////////////////////////////////////////////////////////////////////////////
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Tracks system time.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
 #include "systimer.h"
 
 #ifndef THYME_STANDALONE
 // CPUDetect uses this in static init, breaks if that is initialised before this, define instantiates where used.
-//SysTimeClass &g_theSysTimer = Make_Global<SysTimeClass>(0x00A66B30);
+// SysTimeClass &g_theSysTimer = Make_Global<SysTimeClass>(0x00A66B30);
 #else
 SysTimeClass g_theSysTimer;
 #endif
@@ -44,14 +35,14 @@ int SysTimeClass::Get()
     static bool _is_init;
 #endif
 
-    if ( !_is_init ) {
+    if (!_is_init) {
         Reset();
         _is_init = true;
     }
 
     int time = Time_Func();
 
-    if ( time < m_startTime ) {
+    if (time < m_startTime) {
         return m_negTime + time;
     }
 

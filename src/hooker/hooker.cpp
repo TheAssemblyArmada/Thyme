4ec5b6fdec6c16046c1fb9281a76ac145c5a056b
OmniBlade <nadia_xy@hotmail.com>
2018-03-06 22:50:16 +0000

    Refines hooking code to have sensible fallbacks for standalone.

--- src/hooker/hooker.cpp
+++ src/hooker/hooker.cpp
@@ -1,35 +1,26 @@
-////////////////////////////////////////////////////////////////////////////////
-//                               --  THYME  --                                //
-////////////////////////////////////////////////////////////////////////////////
-//
-//  Project Name:: THYME
-//
-//          File:: HOOKER.CPP
-//
-//        Author:: OmniBlade
-//
-//  Contributors:: CCHyper, Saberhawk, jonwil, LRFLEW
-//
-//   Description:: Originally based on work by the Tiberian Technologies team
-//                 to hook Renegade, rewritten based on work by LRFLEW for
-//                 OpenMC2. Provides methods for accessing data and functions in
-//                 an existing binary and replacing functions with new 
-//                 implementations from an injected DLL.
-//
-//       License:: Thyme is free software: you can redistribute it and/or 
-//                 modify it under the terms of the GNU General Public License 
-//                 as published by the Free Software Foundation, either version 
-//                 2 of the License, or (at your option) any later version.
-//
-//                 A full copy of the GNU General Public License can be found in
-//                 LICENSE
-//                 
-//
-////////////////////////////////////////////////////////////////////////////////
+/**
+* @file
+*
+* @author CCHyper
+* @author jonwil
+* @author LRFLEW
+* @author OmniBlade
+* @author Saberhawk
+*
+* @brief Hooking system for interacting with original binary.
+*
+* @copyright Thyme is free software: you can redistribute it and/or
+*            modify it under the terms of the GNU General Public License
+*            as published by the Free Software Foundation, either version
+*            2 of the License, or (at your option) any later version.
+*            A full copy of the GNU General Public License can be found in
+*            LICENSE
+*
+* Originally based on work by the Tiberian Technologies team to hook Renegade, rewritten based on work by LRFLEW for
+* OpenMC2. Provides methods for accessing data and functions in an existing binary and replacing functions with new
+* implementations from an injected DLL.
+*/
 
-////////////////////////////////////////////////////////////////////////////////
-//  Includes
-////////////////////////////////////////////////////////////////////////////////
 #include "hooker.h"
 
 static const int nBinarySize = 0x0062F000;  //Size of game binary

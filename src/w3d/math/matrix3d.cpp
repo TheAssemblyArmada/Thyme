272b62db5901daa59537b0218655376153da4834
OmniBlade <nadia_xy@hotmail.com>
2018-03-13 13:12:12 +0000

    Applies some formatting to Matrix3D.

--- src/w3d/math/matrix3d.cpp
+++ src/w3d/math/matrix3d.cpp
@@ -1,45 +1,35 @@
-////////////////////////////////////////////////////////////////////////////////
-//                               --  THYME  --                                //
-////////////////////////////////////////////////////////////////////////////////
-//
-//  Project Name:: Thyme
-//
-//          File:: MATRIX3D.H
-//
-//        Author:: Tiberian Technologies
-//
-//  Contributors:: OmniBlade
-//
-//   Description:: 4x3 Matrix class with an implicit 4th row.
-//
-//       License:: Thyme is free software: you can redistribute it and/or 
-//                 modify it under the terms of the GNU General Public License 
-//                 as published by the Free Software Foundation, either version 
-//                 2 of the License, or (at your option) any later version.
-//
-//                 A full copy of the GNU General Public License can be found in
-//                 LICENSE
-//
-////////////////////////////////////////////////////////////////////////////////
+/**
+ * @file
+ *
+ * @author Tiberian Technologies
+ * @author OmniBlade
+ *
+ * @brief 4x3 Matrix class with an implicit 4th row for 4x4 matrix math.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
 #include "matrix3d.h"
 #include "gamemath.h"
 #include "matrix3.h"
 #include "plane.h"
 #include "quaternion.h"
 
-Matrix3D Matrix3D::Identity(1, 0, 0, 0,
-    0, 1, 0, 0,
-    0, 0, 1, 0
-);
+using GameMath::Atan2;
+using GameMath::Fabs;
+using GameMath::Inv_Sqrt;
+using GameMath::Sqrt;
 
-Matrix3D::Matrix3D()
-{
-}
+Matrix3D Matrix3D::Identity(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0);
 
-Matrix3D::Matrix3D(
-    float _11, float _12, float _13, float _14,
-    float _21, float _22, float _23, float _24,
-    float _31, float _32, float _33, float _34)
+Matrix3D::Matrix3D() {}
+
+Matrix3D::Matrix3D(float _11, float _12, float _13, float _14, float _21, float _22, float _23, float _24, float _31,
+    float _32, float _33, float _34)
 {
     this->Row[0] = Vector4(_11, _12, _13, _14);
     this->Row[1] = Vector4(_21, _22, _23, _24);
@@ -52,12 +42,29 @@ int Matrix3D::Is_Orthogonal() const
     Vector3 y(Row[1].X, Row[1].Y, Row[1].Z);
     Vector3 z(Row[2].X, Row[2].Y, Row[2].Z);
 
-    if ( Vector3::Dot_Product(x, y) > GAMEMATH_EPSILON ) return 0;
-    if ( Vector3::Dot_Product(y, z) > GAMEMATH_EPSILON ) return 0;
-    if ( Vector3::Dot_Product(z, x) > GAMEMATH_EPSILON ) return 0;
-    if ( GameMath::Fabs(x.Length2() - 1.0f) > GAMEMATH_EPSILON ) return 0;
-    if ( GameMath::Fabs(y.Length2() - 1.0f) > GAMEMATH_EPSILON ) return 0;
-    if ( GameMath::Fabs(z.Length2() - 1.0f) > GAMEMATH_EPSILON ) return 0;
+    if (Vector3::Dot_Product(x, y) > GAMEMATH_EPSILON) {
+        return 0;
+    }
+
+    if (Vector3::Dot_Product(y, z) > GAMEMATH_EPSILON) {
+        return 0;
+    }
+
+    if (Vector3::Dot_Product(z, x) > GAMEMATH_EPSILON) {
+        return 0;
+    }
+
+    if (Fabs(x.Length2() - 1.0f) > GAMEMATH_EPSILON) {
+        return 0;
+    }
+
+    if (Fabs(y.Length2() - 1.0f) > GAMEMATH_EPSILON) {
+        return 0;
+    }
+
+    if (Fabs(z.Length2() - 1.0f) > GAMEMATH_EPSILON) {
+        return 0;
+    }
 
     return 1;
 }
@@ -83,17 +90,17 @@ void Matrix3D::Get_Orthogonal_Inverse(Matrix3D &inv) const
 
 float Matrix3D::Get_X_Rotation() const
 {
-    return GameMath::Atan2(Row[2][1], Row[1][1]);
+    return Atan2(Row[2][1], Row[1][1]);
 }
 
 float Matrix3D::Get_Y_Rotation() const
 {
-    return GameMath::Atan2(Row[0][2], Row[2][2]);
+    return Atan2(Row[0][2], Row[2][2]);
 }
 
 float Matrix3D::Get_Z_Rotation() const
 {
-    return GameMath::Atan2(Row[1][0], Row[0][0]);
+    return Atan2(Row[1][0], Row[0][0]);
 }
 
 void Matrix3D::Look_At(const Vector3 &p, const Vector3 &t, float roll)
@@ -105,9 +112,9 @@ void Matrix3D::Look_At(const Vector3 &p, const Vector3 &t, float roll)
     dy = (t[1] - p[1]);
     dz = (t[2] - p[2]);
     float rad2 = dx * dx + dy * dy;
-    float len = (float)GameMath::Sqrt(rad2);
+    float len = (float)Sqrt(rad2);
 
-    if ( rad2 != 0.0f ) {
+    if (rad2 != 0.0f) {
         float inv_len = 1.0f / len;
         siny = dy * inv_len;
         cosy = dx * inv_len;
@@ -118,8 +125,8 @@ void Matrix3D::Look_At(const Vector3 &p, const Vector3 &t, float roll)
 
     rad2 += dz * dz;
 
-    if ( rad2 != 0.0f ) {
-        float inv_len2 = (float)GameMath::Inv_Sqrt(rad2);
+    if (rad2 != 0.0f) {
+        float inv_len2 = (float)Inv_Sqrt(rad2);
         sinp = dz * inv_len2;
         cosp = len * inv_len2;
     } else {
@@ -127,9 +134,15 @@ void Matrix3D::Look_At(const Vector3 &p, const Vector3 &t, float roll)
         cosp = 1.0f;
     }
 
-    Row[0].X = 0.0f;    Row[0].Y = 0.0f;    Row[0].Z = -1.0f;
-    Row[1].X = -1.0f;    Row[1].Y = 0.0f;    Row[1].Z = 0.0f;
-    Row[2].X = 0.0f;    Row[2].Y = 1.0f;    Row[2].Z = 0.0f;
+    Row[0].X = 0.0f;
+    Row[0].Y = 0.0f;
+    Row[0].Z = -1.0f;
+    Row[1].X = -1.0f;
+    Row[1].Y = 0.0f;
+    Row[1].Z = 0.0f;
+    Row[2].X = 0.0f;
+    Row[2].Y = 1.0f;
+    Row[2].Z = 0.0f;
     Row[0].W = p.X;
     Row[1].W = p.Y;
     Row[2].W = p.Z;
@@ -138,7 +151,6 @@ void Matrix3D::Look_At(const Vector3 &p, const Vector3 &t, float roll)
     Rotate_X(sinp, cosp);
     Rotate_Z(-roll);
 }
-
 
 void Matrix3D::Obj_Look_At(const Vector3 &p, const Vector3 &t, float roll)
 {
@@ -149,10 +161,10 @@ void Matrix3D::Obj_Look_At(const Vector3 &p, const Vector3 &t, float roll)
     dx = (t[0] - p[0]);
     dy = (t[1] - p[1]);
     dz = (t[2] - p[2]);
-    len1 = (float)sqrt(dx*dx + dy*dy + dz*dz);
-    len2 = (float)sqrt(dx*dx + dy*dy);
+    len1 = (float)Sqrt(dx * dx + dy * dy + dz * dz);
+    len2 = (float)Sqrt(dx * dx + dy * dy);
 
-    if ( len1 != 0.0f ) {
+    if (len1 != 0.0f) {
         sinp = dz / len1;
         cosp = len2 / len1;
     } else {
@@ -160,7 +172,7 @@ void Matrix3D::Obj_Look_At(const Vector3 &p, const Vector3 &t, float roll)
         cosp = 1.0f;
     }
 
-    if ( len2 != 0.0f ) {
+    if (len2 != 0.0f) {
         siny = dy / len2;
         cosy = dx / len2;
     } else {
@@ -212,18 +224,14 @@ void Matrix3D::Set_Rotation(const Quaternion &q)
 
 Vector3 Matrix3D::operator*(const Vector3 &vector) const
 {
-    return Vector3
-    (
-        Row[0].X * vector.X + Row[0].Y * vector.Y + Row[0].Z * vector.Z + Row[0].W,
+    return Vector3(Row[0].X * vector.X + Row[0].Y * vector.Y + Row[0].Z * vector.Z + Row[0].W,
         Row[1].X * vector.X + Row[1].Y * vector.Y + Row[1].Z * vector.Z + Row[1].W,
-        Row[2].X * vector.X + Row[2].Y * vector.Y + Row[2].Z * vector.Z + Row[2].W
-    );
+        Row[2].X * vector.X + Row[2].Y * vector.Y + Row[2].Z * vector.Z + Row[2].W);
 }
 
 Matrix3D &Matrix3D::operator*=(const Matrix3D &matrix)
 {
-    Set(
-        Row[0].X * matrix.Row[0].X + Row[0].Y * matrix.Row[1].X + Row[0].Z * matrix.Row[2].X,
+    Set(Row[0].X * matrix.Row[0].X + Row[0].Y * matrix.Row[1].X + Row[0].Z * matrix.Row[2].X,
         Row[0].X * matrix.Row[0].Y + Row[0].Y * matrix.Row[1].Y + Row[0].Z * matrix.Row[2].Y,
         Row[0].X * matrix.Row[0].Z + Row[0].Y * matrix.Row[1].Z + Row[0].Z * matrix.Row[2].Z,
         Row[0].X * matrix.Row[0].W + Row[0].Y * matrix.Row[1].W + Row[0].Z * matrix.Row[2].W + Row[0].W,
@@ -236,8 +244,7 @@ Matrix3D &Matrix3D::operator*=(const Matrix3D &matrix)
         Row[2].X * matrix.Row[0].X + Row[2].Y * matrix.Row[1].X + Row[2].Z * matrix.Row[2].X,
         Row[2].X * matrix.Row[0].Y + Row[2].Y * matrix.Row[1].Y + Row[2].Z * matrix.Row[2].Y,
         Row[2].X * matrix.Row[0].Z + Row[2].Y * matrix.Row[1].Z + Row[2].Z * matrix.Row[2].Z,
-        Row[2].X * matrix.Row[0].W + Row[2].Y * matrix.Row[1].W + Row[2].Z * matrix.Row[2].W + Row[2].W
-    );
+        Row[2].X * matrix.Row[0].W + Row[2].Y * matrix.Row[1].W + Row[2].Z * matrix.Row[2].W + Row[2].W);
 
     return *this;
 }
@@ -295,7 +302,7 @@ void Matrix3D::Multiply(const Matrix3D &A, const Matrix3D &B, Matrix3D *set_res)
     Matrix3D *Aptr;
     float tmp1, tmp2, tmp3;
 
-    if ( set_res == &A ) {
+    if (set_res == &A) {
         tmp = A;
         Aptr = &tmp;
     } else {
@@ -335,12 +342,12 @@ void Matrix3D::Transform_Min_Max_AABox(const Vector3 &min, const Vector3 &max, V
     set_min->Y = set_max->Y = Row[1][3];
     set_min->Z = set_max->Z = Row[2][3];
 
-    for ( int i = 0; i < 3; i++ ) {
-        for ( int j = 0; j < 3; j++ ) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             tmp0 = Row[i][j] * min[j];
             tmp1 = Row[i][j] * max[j];
 
-            if ( tmp0 < tmp1 ) {
+            if (tmp0 < tmp1) {
                 (*set_min)[i] += tmp0;
                 (*set_max)[i] += tmp1;
             } else {
@@ -351,33 +358,43 @@ void Matrix3D::Transform_Min_Max_AABox(const Vector3 &min, const Vector3 &max, V
     }
 }
 
-void Matrix3D::Transform_Center_Extent_AABox(const Vector3 &center, const Vector3 &extent, Vector3 *set_center, Vector3 *set_extent) const
+void Matrix3D::Transform_Center_Extent_AABox(
+    const Vector3 &center, const Vector3 &extent, Vector3 *set_center, Vector3 *set_extent) const
 {
-    for ( int i = 0; i < 3; i++ ) {
+    for (int i = 0; i < 3; i++) {
         (*set_center)[i] = Row[i][3];
         (*set_extent)[i] = 0.0f;
 
-        for ( int j = 0; j < 3; j++ ) {
+        for (int j = 0; j < 3; j++) {
             (*set_center)[i] += Row[i][j] * center[j];
-            (*set_extent)[i] += GameMath::Fabs(Row[i][j] * extent[j]);
+            (*set_extent)[i] += Fabs(Row[i][j] * extent[j]);
         }
     }
 }
 
-Matrix3D Matrix3D::Reflect_Plane(const PlaneClass& _plane)
+Matrix3D Matrix3D::Reflect_Plane(const PlaneClass &_plane)
 {
     PlaneClass plane = _plane;
     plane.Normalize();
 
     Matrix3D temp(true);
-    temp[0][0] = -2 * plane.N.X * plane.N.X + 1;    temp[0][1] = -2 * plane.N.X * plane.N.Y;        temp[0][2] = -2 * plane.N.X * plane.N.Z;        temp[0][3] = 2 * plane.N.X * plane.D;
-    temp[1][0] = -2 * plane.N.Y * plane.N.X;        temp[1][1] = -2 * plane.N.Y * plane.N.Y + 1;    temp[1][2] = -2 * plane.N.Y * plane.N.Z;        temp[0][3] = 2 * plane.N.Y * plane.D;
-    temp[2][0] = -2 * plane.N.Z * plane.N.X;        temp[2][1] = -2 * plane.N.Z * plane.N.Y;        temp[2][2] = -2 * plane.N.Z * plane.N.Z + 1;    temp[2][3] = 2 * plane.N.Z * plane.D;
-    
+    temp[0][0] = -2 * plane.N.X * plane.N.X + 1;
+    temp[0][1] = -2 * plane.N.X * plane.N.Y;
+    temp[0][2] = -2 * plane.N.X * plane.N.Z;
+    temp[0][3] = 2 * plane.N.X * plane.D;
+    temp[1][0] = -2 * plane.N.Y * plane.N.X;
+    temp[1][1] = -2 * plane.N.Y * plane.N.Y + 1;
+    temp[1][2] = -2 * plane.N.Y * plane.N.Z;
+    temp[0][3] = 2 * plane.N.Y * plane.D;
+    temp[2][0] = -2 * plane.N.Z * plane.N.X;
+    temp[2][1] = -2 * plane.N.Z * plane.N.Y;
+    temp[2][2] = -2 * plane.N.Z * plane.N.Z + 1;
+    temp[2][3] = 2 * plane.N.Z * plane.D;
+
     return temp;
 };
 
-PlaneClass Matrix3D::Transform_Plane(const PlaneClass& plane) const
+PlaneClass Matrix3D::Transform_Plane(const PlaneClass &plane) const
 {
     PlaneClass out;
     out.N.X = Row[0][0] * plane.N.X + Row[1][0] * plane.N.Y + Row[2][0] * plane.N.Z;
@@ -390,7 +407,7 @@ PlaneClass Matrix3D::Transform_Plane(const PlaneClass& plane) const
 
 bool Matrix3D::Solve_Linear_System(Matrix3D &system)
 {
-    if ( system[0][0] == 0.0f ) {
+    if (system[0][0] == 0.0f) {
         return false;
     }
 
@@ -398,14 +415,14 @@ bool Matrix3D::Solve_Linear_System(Matrix3D &system)
     system[1] -= system[1][0] * system[0];
     system[2] -= system[2][0] * system[0];
 
-    if ( system[1][1] == 0.0f ) {
+    if (system[1][1] == 0.0f) {
         return false;
     }
 
     system[1] *= 1.0f / system[1][1];
     system[2] -= system[2][1] * system[1];
 
-    if ( system[2][2] == 0.0f ) {
+    if (system[2][2] == 0.0f) {
         return false;
     }
 
@@ -426,7 +443,7 @@ void Matrix3D::Re_Orthogonalize()
     Vector3::Cross_Product(z, x, &y);
     float len = x.Length();
 
-    if ( len < GAMEMATH_EPSILON ) {
+    if (len < GAMEMATH_EPSILON) {
         Make_Identity();
 
         return;
@@ -436,7 +453,7 @@ void Matrix3D::Re_Orthogonalize()
 
     len = y.Length();
 
-    if ( len < GAMEMATH_EPSILON ) {
+    if (len < GAMEMATH_EPSILON) {
         Make_Identity();
 
         return;
@@ -446,7 +463,7 @@ void Matrix3D::Re_Orthogonalize()
 
     len = z.Length();
 
-    if ( len < GAMEMATH_EPSILON ) {
+    if (len < GAMEMATH_EPSILON) {
         Make_Identity();
 
         return;

3b95807028160ab8292b9196007e1c86f9f74e12
OmniBlade <nadia_xy@hotmail.com>
2018-03-20 22:58:39 +0000

    Implements skelton of ProductionPrerequisite.

---
+++ src/game/common/rts/productionprerequisite.h
@@ -0,0 +1,45 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Tracks prerequisites needed for a thing to be built.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef PRODUCTIONPREREQUISITE_H
+#define PRODUCTIONPREREQUISITE_H
+
+#include "always.h"
+#include "asciistring.h"
+#include "science.h"
+#include <vector>
+
+class ThingTemplate;
+
+class ProductionPrerequisite
+{
+    struct PrereqUnitRec
+    {
+        ThingTemplate *unit;
+        int flags;
+        AsciiString name;
+    };
+public:
+    ProductionPrerequisite() {}
+    ~ProductionPrerequisite() {}
+
+    // TODO member functions.
+private:
+    std::vector<PrereqUnitRec> m_prereqUnit;
+    std::vector<ScienceType> m_prereqScience;
+};
+
+#endif // PRODUCTIONPREREQUISITE_H

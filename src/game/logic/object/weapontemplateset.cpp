ea38cb092347eb9573b076a3e3d1fdcb3ed09b00
OmniBlade <nadia_xy@hotmail.com>
2018-03-21 22:26:39 +0000

    Implements WeaponTemplateSet.

---
+++ src/game/logic/object/weapontemplateset.cpp
@@ -0,0 +1,135 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Templated information for object weapon sets.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "weapontemplateset.h"
+#include <cstddef>
+
+const char *BitFlags<WEAPONSET_COUNT>::s_bitNamesList[] = {
+    "VETERAN",
+    "ELITE",
+    "HERO",
+    "PLAYER_UPGRADE",
+    "CRATEUPGRADE_ONE",
+    "CRATEUPGRADE_TWO",
+    "VEHICLE_HIJACK",
+    "CARBOMB",
+    "MINE_CLEARING_DETAIL",
+    "WEAPON_RIDER1",
+    "WEAPON_RIDER2",
+    "WEAPON_RIDER3",
+    "WEAPON_RIDER4",
+    "WEAPON_RIDER5",
+    "WEAPON_RIDER6",
+    "WEAPON_RIDER7",
+    "WEAPON_RIDER8",
+    nullptr};
+
+/**
+ * @brief Clears the template set.
+ *
+ * 0x00605E00
+ */
+void WeaponTemplateSet::Clear()
+{
+    m_shareWeaponReloadTime = false;
+    m_weaponLockSharedAcrossSets = false;
+    m_conditions.Clear();
+
+    for (int i = 0; i < WEAPONSLOT_COUNT; ++i) {
+        m_preferredAgainst[i].Clear();
+        m_autoChooseMask[i] = 0;
+        m_template[i] = nullptr;
+    }
+}
+
+/**
+ * @brief Checks if the set contains any weapons.
+ *
+ * 0x00605E40
+ */
+bool WeaponTemplateSet::Has_Any_Weapon()
+{
+    for (int i = 0; i < WEAPONSLOT_COUNT; ++i) {
+        if (m_template[i] != nullptr) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/**
+ * @brief Check if this set matches a given weapon condition?
+ *
+ * 0x00606190
+ */
+bool WeaponTemplateSet::Test_Weapon_Set_Flag(WeaponSetType set)
+{
+    return m_conditions.Get(set);
+}
+
+/**
+ * @brief Parse a weapon template set from an ini file.
+ *
+ * 0x006060B0
+ */
+void WeaponTemplateSet::Parse_Weapon_Template_Set(INI *ini, const ThingTemplate *type)
+{
+    static FieldParse _parse_table[] = {
+        {"Conditions", &BitFlags<WEAPONSET_COUNT>::Parse_INI, nullptr, offsetof(WeaponTemplateSet, m_conditions)},
+        {"Weapon", &WeaponTemplateSet::Parse_Weapon, nullptr, 0},
+        {"AutoChooseSources", &WeaponTemplateSet::Parse_Auto_Choose, nullptr, 0},
+        {"PreferredAgainst", &WeaponTemplateSet::Parse_Preferred_Against, nullptr, 0},
+        {"ShareWeaponReloadTime", &INI::Parse_Bool, nullptr, offsetof(WeaponTemplateSet, m_shareWeaponReloadTime)},
+        {"WeaponLockSharedAcrossSets", &INI::Parse_Bool, nullptr, offsetof(WeaponTemplateSet, m_weaponLockSharedAcrossSets)},
+        {nullptr, nullptr, nullptr, 0}};
+
+    ini->Init_From_INI(this, _parse_table);
+    m_type = type;
+}
+
+/**
+ * @brief Parse a weapon from an ini file.
+ *
+ * 0x00605E60
+ */
+void WeaponTemplateSet::Parse_Weapon(INI *ini, void *formal, void *store, const void *user_data)
+{
+    // TODO requires WeaponTemplate parser
+#ifndef THYME_STANDALONE
+    Call_Function<void, INI *, void *, void *, const void *>(0x00605E60, ini, formal, store, user_data);
+#endif
+}
+
+/**
+ * @brief Parse auto choose mask from an ini file.
+ *
+ * 0x00605E90
+ */
+void WeaponTemplateSet::Parse_Auto_Choose(INI *ini, void *formal, void *store, const void *user_data)
+{
+    int index = INI::Scan_IndexList(ini->Get_Next_Token(), g_weaponSlotNames);
+    INI::Parse_Bitstring32(ini, formal, &static_cast<WeaponTemplateSet *>(formal)->m_autoChooseMask[index], g_commandSourceMaskNames);
+}
+
+/**
+ * @brief Parse preferred against KindOf from an ini file.
+ *
+ * 0x00605ED0
+ */
+void WeaponTemplateSet::Parse_Preferred_Against(INI *ini, void *formal, void *store, const void *user_data)
+{
+    int index = INI::Scan_IndexList(ini->Get_Next_Token(), g_weaponSlotNames);
+    BitFlags<KINDOF_COUNT>::Parse_INI(ini, formal, &static_cast<WeaponTemplateSet *>(formal)->m_preferredAgainst[index], user_data);
+}
---
+++ src/game/logic/object/weapontemplateset.h
@@ -0,0 +1,74 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Templated information for object weapon sets.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef WEAPONTEMPLATESET_H
+#define WEAPONTEMPLATESET_H
+
+#include "always.h"
+#include "bitflags.h"
+#include "gametype.h"
+#include "ini.h"
+#include "kindof.h"
+
+class ThingTemplate;
+class WeaponTemplate;
+
+enum WeaponSetType
+{
+    WEAPONSET_VETERAN,
+    WEAPONSET_ELITE,
+    WEAPONSET_HERO,
+    WEAPONSET_PLAYER_UPGRADE,
+    WEAPONSET_CRATEUPGRADE_ONE,
+    WEAPONSET_CRATEUPGRADE_TWO,
+    WEAPONSET_VEHICLE_HIJACK,
+    WEAPONSET_CARBOMB,
+    WEAPONSET_MINE_CLEARING_DETAIL,
+    WEAPONSET_WEAPON_RIDER1,
+    WEAPONSET_WEAPON_RIDER2,
+    WEAPONSET_WEAPON_RIDER3,
+    WEAPONSET_WEAPON_RIDER4,
+    WEAPONSET_WEAPON_RIDER5,
+    WEAPONSET_WEAPON_RIDER6,
+    WEAPONSET_WEAPON_RIDER7,
+    WEAPONSET_WEAPON_RIDER8,
+    WEAPONSET_COUNT,
+};
+
+class WeaponTemplateSet
+{
+public:
+    WeaponTemplateSet() : m_type(nullptr) { Clear(); }
+    void Clear();
+    bool Has_Any_Weapon();
+    bool Test_Weapon_Set_Flag(WeaponSetType set);
+    void Parse_Weapon_Template_Set(INI *ini, const ThingTemplate *type);
+
+    static void Parse_Weapon(INI *ini, void *formal, void *store, const void *user_data);
+    static void Parse_Auto_Choose(INI *ini, void *formal, void *store, const void *user_data);
+    static void Parse_Preferred_Against(INI *ini, void *formal, void *store, const void *user_data);
+
+private:
+    const ThingTemplate *m_type;
+    BitFlags<WEAPONSET_COUNT> m_conditions;
+    WeaponTemplate *m_template[WEAPONSLOT_COUNT];
+    uint32_t m_autoChooseMask[WEAPONSLOT_COUNT];
+    BitFlags<KINDOF_COUNT> m_preferredAgainst[WEAPONSLOT_COUNT];
+    bool m_shareWeaponReloadTime;
+    bool m_weaponLockSharedAcrossSets;
+};
+
+#endif // WEAPONTEMPLATESET_H

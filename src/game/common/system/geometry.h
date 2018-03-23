12f1fca26832aa7fbaf8dcd12394a441766b233e
OmniBlade <nadia_xy@hotmail.com>
2018-03-23 13:23:05 +0000

    Implements more GeometryInfo methods.
    Thanks to tomsons26 for some of this.

--- src/game/common/system/geometry.h
+++ src/game/common/system/geometry.h
@@ -18,6 +18,8 @@
 #define GEOMETRY_H
 
 #include "always.h"
+#include "coord.h"
+#include "ini.h"
 #include "snapshot.h"
 
 enum GeometryType : int32_t
@@ -38,9 +40,24 @@ public:
     virtual void Load_Post_Process() {}
 
     void Set(GeometryType type, bool small, float height, float major_radius, float minor_radius);
-    void Calc_Bounding_Box_Stuff();
+    void Calc_Bounding_Stuff();
+    void Set_Max_Height_Above_Position(float max_height);
+    float Get_Max_Height_Above_Position();
+    float Get_Max_Height_Below_Position();
+    float Get_ZDelta_To_Center_Position();
+    void Get_Center_Position(const Coord3D &pos, Coord3D &result);
+    float Get_Footprint_Area();
+    void Expand_Footprint(float expand_by);
+    float Is_Intersected_By_Line_Segment(Coord3D &loc, Coord3D &from, Coord3D &to);
+
     // TODO none virtual functions.
 
+    static void Parse_Geometry_MinorRadius(INI *ini, void *formal, void *store, void *user_data);
+    static void Parse_Geometry_MajorRadius(INI *ini, void *formal, void *store, void *user_data);
+    static void Parse_Geometry_Height(INI *ini, void *formal, void *store, void *user_data);
+    static void Parse_Geometry_IsSmall(INI *ini, void *formal, void *store, void *user_data);
+    static void Parse_Geometry_Type(INI *ini, void *formal, void *store, void *user_data);
+    
 private:
     GeometryType m_type;
     bool m_isSmall;

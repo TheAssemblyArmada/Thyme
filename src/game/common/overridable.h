c6f6a18b3f876cec60b1be49e0667561be42ec00
OmniBlade <nadia_xy@hotmail.com>
2018-05-12 23:03:15 +0100

    Applies clang-format to overridable.h.

--- src/game/common/overridable.h
+++ src/game/common/overridable.h
@@ -1,27 +1,17 @@
-////////////////////////////////////////////////////////////////////////////////
-//                               --  THYME  --                                //
-////////////////////////////////////////////////////////////////////////////////
-//
-//  Project Name:: Thyme
-//
-//          File:: OVERRIDABLE.H
-//
-//        Author:: OmniBlade
-//
-//  Contributors:: 
-//
-//   Description:: Appears to provide a base class for objects that can be
-//                 Overridden.
-//
-//       License:: Thyme is free software: you can redistribute it and/or 
-//                 modify it under the terms of the GNU General Public License 
-//                 as published by the Free Software Foundation, either version 
-//                 2 of the License, or (at your option) any later version.
-//
-//                 A full copy of the GNU General Public License can be found in
-//                 LICENSE
-//
-////////////////////////////////////////////////////////////////////////////////
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Base class for overridable objects.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
 #pragma once
 
 #include "mempoolobj.h"
@@ -48,7 +38,7 @@ inline Overridable *Overridable::Get_Final_Override()
 {
     Overridable *retval = this;
 
-    while ( retval->m_next != nullptr ) {
+    while (retval->m_next != nullptr) {
         retval = retval->m_next;
     }
 
@@ -57,13 +47,13 @@ inline Overridable *Overridable::Get_Final_Override()
 
 inline Overridable *Overridable::Delete_Overrides()
 {
-    if ( m_isAllocated ) {
+    if (m_isAllocated) {
         Delete_Instance(this);
 
         return nullptr;
     }
 
-    if ( m_next != nullptr ) {
+    if (m_next != nullptr) {
         m_next = m_next->Delete_Overrides();
     }
 
@@ -72,7 +62,7 @@ inline Overridable *Overridable::Delete_Overrides()
 
 inline void Overridable::Add_Override(Overridable *obj)
 {
-    if ( m_next != nullptr ) {
+    if (m_next != nullptr) {
         Get_Final_Override()->m_next = obj;
     } else {
         m_next = obj;

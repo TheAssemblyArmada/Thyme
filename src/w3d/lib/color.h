3777a51666e47eaa16ad0c39e6e428c488e699c6
OmniBlade <nadia_xy@hotmail.com>
2018-05-17 22:50:56 +0100

    Adds RGBColorKeyFrame struct for particle systems.

--- src/w3d/lib/color.h
+++ src/w3d/lib/color.h
@@ -1,29 +1,20 @@
-////////////////////////////////////////////////////////////////////////////////
-//                               --  THYME  --                                //
-////////////////////////////////////////////////////////////////////////////////
-//
-//  Project Name:: Thyme
-//
-//          File:: COLOR.H
-//
-//        Author:: OmniBlade
-//
-//  Contributors:: 
-//
-//   Description:: Color objects.
-//
-//       License:: Thyme is free software: you can redistribute it and/or 
-//                 modify it under the terms of the GNU General Public License 
-//                 as published by the Free Software Foundation, either version 
-//                 2 of the License, or (at your option) any later version.
-//
-//                 A full copy of the GNU General Public License can be found in
-//                 LICENSE
-//
-////////////////////////////////////////////////////////////////////////////////
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Objects for containing color values.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
 #pragma once
 
-#include <cstdint>
+#include "always.h"
 
 // Pack individual bytes into a dword
 inline uint32_t Make_Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
@@ -55,4 +46,10 @@ public:
     uint32_t green;
     uint32_t blue;
     uint32_t alpha;
+};
+
+struct RGBColorKeyframe
+{
+    RGBColor color;
+    uint32_t frame;
 };

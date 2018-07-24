1d45c52ec745fa8408ede01047e3979d4bfdbcf7
OmniBlade <nadia_xy@hotmail.com>
2018-07-24 12:59:10 +0100

    Implements base classes for playing videos.

---
+++ src/game/client/videoplayer/videobuffer.cpp
@@ -0,0 +1,62 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Base class interface for video buffers.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */ 
+#include "videobuffer.h"
+
+/**
+ * 0x0051AA80
+ */
+VideoBuffer::VideoBuffer(VideoBuffer::Type type) :
+    m_xPos(0),
+    m_yPos(0),
+    m_width(0),
+    m_height(0),
+    m_textureWidth(0),
+    m_textureHeight(0),
+    m_pitch(0),
+    m_format(type)
+{
+    if (type >= TYPE_COUNT || type < TYPE_UNKNOWN) {
+        m_format = TYPE_UNKNOWN;   
+    }
+}
+
+/**
+ * @brief Frees the buffer. 
+ *
+ * 0x0051ABA0
+ */
+void VideoBuffer::Free()
+{
+    m_width = 0;
+    m_height = 0;
+    m_textureWidth = 0;
+    m_textureHeight = 0;
+}
+
+/**
+ * @brief Generates the rectangle the video will occupy (I think).
+ *
+ * 0x0051AAE0
+ */
+RectClass VideoBuffer::Rect(float x1, float y1, float x2, float y2)
+{
+    if (Valid()) {
+        float width = m_width / m_textureWidth;
+        float height = m_height / m_textureHeight;
+        return RectClass(width * x1, height *y1, width * x2, height * y2);
+    }
+
+    return RectClass(0.0f, 0.0f, 0.0f, 0.0f);
+}
---
+++ src/game/client/videoplayer/videostream.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Virtual interface for video streams.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */ 
+#include "videostream.h"
+#include "videoplayer.h"
+
+/**
+ * 0x0051B1F0
+ */
+VideoStream::~VideoStream()
+{
+    if (m_player != nullptr) {
+        m_player->Remove_Stream(this);
+        m_player = nullptr;
+    }
+}

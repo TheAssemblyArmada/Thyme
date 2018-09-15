417c6254cb7cb97347ca0ffe5e4340aeb2c4dc0a
OmniBlade <nadia_xy@hotmail.com>
2018-09-15 23:11:54 +0100

    Adds RefCountClass.

---
+++ src/w3d/lib/refcount.h
@@ -0,0 +1,81 @@
+/**
+ * @file
+ *
+ * @author tomsons26
+ *
+ * @brief Base class for objects that need to be reference counted.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+class RefCountClass
+{
+public:
+    RefCountClass() : m_numRefs(1)
+    {
+#ifdef THYME_DEBUG
+        Inc_Total_Refs(this);
+#endif
+    }
+
+    RefCountClass(const RefCountClass &) : m_numRefs(1)
+    {
+#ifdef THYME_DEBUG
+        Inc_Total_Refs(this);
+#endif
+    }
+
+    void Add_Ref() { m_numRefs++; }
+    int Num_Refs() { return m_numRefs; }
+    
+    void Release_Ref()
+    {
+#ifdef THYME_DEBUG
+        Dec_Total_Refs(this);
+#endif
+        m_numRefs--;
+
+        if (m_numRefs == 0) {
+            Delete_This();
+        }
+    }
+
+    virtual void Delete_This() { delete this; }
+
+protected:
+    virtual ~RefCountClass() {}
+
+private:
+    static void Inc_Total_Refs(RefCountClass *obj) { m_totalRefs++; }
+    static void Dec_Total_Refs(RefCountClass *obj) { m_totalRefs--; }
+    int m_numRefs;
+    static int m_totalRefs;
+};
+
+inline void Ref_Ptr_Set(RefCountClass *&src, RefCountClass *&dst)
+{
+    if (src != nullptr) {
+        src->Add_Ref();
+    }
+
+    if (dst != nullptr) {
+        dst->Release_Ref();
+    }
+
+    dst = src;
+}
+
+inline void Ref_Ptr_Release(RefCountClass *&ptr)
+{
+    if (ptr != nullptr) {
+        ptr->Release_Ref();
+    }
+    
+    ptr = nullptr;
+}

4f9c396715f922e9ae39e4abb4f8351a1bfdb5e4
Toms <tomsons26lv@gmail.com>
2018-09-16 23:12:54 +0100

    Adds CastResultsStruct.

---
+++ src/w3d/math/castres.h
@@ -0,0 +1,40 @@
+/**
+ * @file
+ *
+ * @author tomsons26
+ *
+ * @brief Returns the results of a ray cast?
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#include "always.h"
+#include "vector3.h"
+
+struct CastResultStruct
+{
+    CastResultStruct() { Reset(); }
+
+    void Reset()
+    {
+        start_bad = false;
+        fraction = 1.0f;
+        normal.Set(0.0f, 0.0f, 0.0f);
+        surface_type = 0;
+        compute_contact_point = false;
+        contact_point.Set(0.0f, 0.0f, 0.0f);
+    }
+
+    bool start_bad;
+    float fraction;
+    Vector3 normal;
+    uint32_t surface_type;
+    bool compute_contact_point;
+    Vector3 contact_point;
+};

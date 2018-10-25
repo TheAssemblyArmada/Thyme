cd7a9f9fa9d896e0431a0349f8aa6ff2b7d92c12
OmniBlade <nadia_xy@hotmail.com>
2018-10-25 13:22:22 +0100

    Implements ShaderClass.

---
+++ src/w3d/renderer/shader.cpp
@@ -0,0 +1,423 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ * @author tomsons26
+ * @author Tiberium Technologies
+ *
+ * @brief Base class for objects that use shaders.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "shader.h"
+#include "gamedebug.h"
+#include "wwstring.h"
+
+// TODO move these to w3dfile once that file is written.
+struct W3dRGBStruct
+{
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t pad;
+};
+
+struct W3dMaterial3Struct
+{
+    uint32_t attributes;
+    W3dRGBStruct diffuse_color;
+    W3dRGBStruct specular_color;
+    W3dRGBStruct emissive_coeffs;
+    W3dRGBStruct ambient_coeffs;
+    W3dRGBStruct diffuse_coeffs;
+    W3dRGBStruct specular_coeffs;
+    float shininess;
+    float opacity;
+    float translucency;
+    float fog_coeff;
+};
+
+
+// TODO these should be decoded into Shader_Const calls.
+ShaderClass ShaderClass::s_presetOpaqueShader(0x9441B);
+ShaderClass ShaderClass::s_presetAdditiveShader(0x94433);
+ShaderClass ShaderClass::s_presetBumpenvmapShader(0x494C33);
+ShaderClass ShaderClass::s_presetAlphaShader(0x984B3);
+ShaderClass ShaderClass::s_presetMultiplicativeShader(0x90453);
+ShaderClass ShaderClass::s_presetOpaque2DShader(0x94017);
+ShaderClass ShaderClass::s_presetOpaqueSpriteShader(0x94013);
+ShaderClass ShaderClass::s_presetAdditive2DShader(0x94037);
+ShaderClass ShaderClass::s_presetAlpha2DShader(0x980B7);
+ShaderClass ShaderClass::s_presetAdditiveSpriteShader(0x94033);
+ShaderClass ShaderClass::s_presetAlphaSpriteShader(0x980B3);
+ShaderClass ShaderClass::s_presetOpaqueSolidShader(0x8441B);
+ShaderClass ShaderClass::s_presetAdditiveSolidShader(0x84433);
+ShaderClass ShaderClass::s_presetAlphaSolidShader(0x884B3);
+ShaderClass ShaderClass::s_presetATest2DShader(0xD4017);
+ShaderClass ShaderClass::s_presetATestSpriteShader(0xD401B);
+ShaderClass ShaderClass::s_presetATestBlend2DShader(0xD80B7);
+ShaderClass ShaderClass::s_presetATestBlendSpriteShader(0xD80BB);
+ShaderClass ShaderClass::s_presetScreen2DShader(0x94077);
+ShaderClass ShaderClass::s_presetScreenSpriteShader(0x94073);
+ShaderClass ShaderClass::s_presetMultiplicative2DShader(0x90057);
+ShaderClass ShaderClass::s_presetMultiplicativeSpriteShader(0x90053);
+
+
+#ifndef THYME_STANDALONE
+bool &ShaderClass::s_shaderDirty = Make_Global<bool>(0x00A16C1C);
+uint32_t &ShaderClass::s_currentShader = Make_Global<uint32_t>(0x00A4C1B4);
+uint32_t &ShaderClass::s_polygonCullMode = Make_Global<uint32_t>(0x00A16C20);
+#else
+bool ShaderClass::s_shaderDirty = false;
+uint32_t ShaderClass::s_currentShader = 0;
+uint32_t ShaderClass::s_polygonCullMode = 2;
+#endif
+
+void ShaderClass::Init_From_Material3(const W3dMaterial3Struct &material)
+{
+    // TODO figure out what the attribute bits do.
+    if (material.attributes & 1) {
+        Set_Depth_Mask(DEPTH_WRITE_DISABLE);
+        Set_Dst_Blend_Func(DSTBLEND_ONE_MINUS_SRC_ALPHA);
+        Set_Src_Blend_Func(SRCBLEND_SRC_ALPHA);
+    }
+}
+
+void ShaderClass::Enable_Fog(const char *source)
+{
+    switch (Get_Src_Blend_Func()) {
+        case SRCBLEND_ZERO:
+            if (Get_Dst_Blend_Func() == DSTBLEND_SRC_COLOR) {
+                Set_Fog_Func(FOG_WHITE);
+                return;
+            }
+            Report_Unable_To_Fog(source);
+            break;
+        case SRCBLEND_ONE:
+            switch (Get_Dst_Blend_Func()) {
+                case DSTBLEND_ZERO:
+                    Set_Fog_Func(FOG_ENABLE);
+                    return;
+                case DSTBLEND_ONE:
+                case DSTBLEND_ONE_MINUS_SRC_COLOR:
+                    Set_Fog_Func(FOG_SCALE_FRAGMENT);
+                    return;
+            }
+            Report_Unable_To_Fog(source);
+            break;
+        case SRCBLEND_SRC_ALPHA:
+            if (Get_Dst_Blend_Func() == DSTBLEND_ONE_MINUS_SRC_ALPHA) {
+                Set_Fog_Func(FOG_ENABLE);
+                return;
+            }
+            Report_Unable_To_Fog(source);
+            break;
+        case SRCBLEND_ONE_MINUS_SRC_ALPHA:
+            if (Get_Dst_Blend_Func() == DSTBLEND_SRC_ALPHA) {
+                Set_Fog_Func(FOG_ENABLE);
+                return;
+            }
+            Report_Unable_To_Fog(source);
+            break;
+        default:
+            return;
+    }
+}
+
+ShaderClass::StaticSortCategoryType ShaderClass::Get_Static_Sort_Category() const
+{
+    if (Get_Alpha_Test() == ALPHATEST_DISABLE && Get_Dst_Blend_Func() == DSTBLEND_ZERO) {
+        return SSCAT_OPAQUE;
+    }
+    
+    if (Get_Alpha_Test() == MASK_ALPHATEST) {
+        if (Get_Dst_Blend_Func() == DSTBLEND_ZERO
+            || (Get_Dst_Blend_Func() == DSTBLEND_ONE_MINUS_SRC_ALPHA) && Get_Src_Blend_Func() == SRCBLEND_SRC_ALPHA) {
+            return SSCAT_ALPHA_TEST;
+        }
+    }
+
+    if (Get_Src_Blend_Func() == SRCBLEND_ONE) {
+        return SSCAT_OTHER2;
+    }
+
+    if (Get_Dst_Blend_Func() == DSTBLEND_ONE) {
+        return SSCAT_ADDITIVE;
+    }
+
+    return Get_Dst_Blend_Func() == DSTBLEND_ONE_MINUS_SRC_COLOR ? SSCAT_OTHER : SSCAT_OTHER2;
+}
+
+int ShaderClass::Guess_Sort_Level() const
+{
+    switch (Get_Static_Sort_Category()) {
+        case SSCAT_OPAQUE: // Fallthrough.
+        case SSCAT_ALPHA_TEST:
+            return SORT_LEVEL_NONE;
+        case SSCAT_ADDITIVE:
+            return SORT_LEVEL_BIN3;
+        case SSCAT_OTHER:
+            return SORT_LEVEL_BIN2;
+        default:
+            return SORT_LEVEL_BIN1;
+    }
+}
+
+void ShaderClass::Get_Description(StringClass &desc)
+{
+    desc = "";
+    switch (Get_Depth_Compare()) {
+        case PASS_NEVER:
+            desc += "DEPTH_COMPARE:PASS_NEVER | ";
+            break;
+        case PASS_LESS:
+            desc += "DEPTH_COMPARE:PASS_LESS | ";
+            break;
+        case PASS_EQUAL:
+            desc += "DEPTH_COMPARE:PASS_EQUAL | ";
+            break;
+        case PASS_LEQUAL:
+            desc += "DEPTH_COMPARE:PASS_LEQUAL | ";
+            break;
+        case PASS_GREATER:
+            desc += "DEPTH_COMPARE:PASS_GREATER | ";
+            break;
+        case PASS_NOTEQUAL:
+            desc += "DEPTH_COMPARE:PASS_NOTEQUAL | ";
+            break;
+        case PASS_GEQUAL:
+            desc += "DEPTH_COMPARE:PASS_GEQUAL | ";
+            break;
+        case PASS_ALWAYS:
+            desc += "DEPTH_COMPARE:PASS_ALWAYS | ";
+            break;
+        default:
+            break;
+    }
+
+    switch (Get_Depth_Mask()) {
+        case DEPTH_WRITE_DISABLE:
+            desc += "DEPTH_WRITE_DISABLE | ";
+            break;
+        case DEPTH_WRITE_ENABLE:
+            desc += "DEPTH_WRITE_ENABLE | ";
+            break;
+        default:
+            break;
+    }
+
+    switch (Get_Color_Mask()) {
+        case DEPTH_WRITE_DISABLE:
+            desc += "COLOR_WRITE_DISABLE | ";
+            break;
+        case COLOR_WRITE_ENABLE:
+            desc += "COLOR_WRITE_ENABLE | ";
+            break;
+        default:
+            break;
+    }
+
+    switch (Get_Dst_Blend_Func()) {
+        case DSTBLEND_ZERO:
+            desc += "DSTBLEND_ZERO | ";
+            break;
+        case DSTBLEND_ONE:
+            desc += "DSTBLEND_ONE | ";
+            break;
+        case DSTBLEND_SRC_COLOR:
+            desc += "DSTBLEND_SRC_COLOR | ";
+            break;
+        case DSTBLEND_ONE_MINUS_SRC_COLOR:
+            desc += "DSTBLEND_ONE_MINUS_SRC_COLOR | ";
+            break;
+        case DSTBLEND_SRC_ALPHA:
+            desc += "DSTBLEND_SRC_ALPHA | ";
+            break;
+        case DSTBLEND_ONE_MINUS_SRC_ALPHA:
+            desc += "DSTBLEND_ONE_MINUS_SRC_ALPHA | ";
+            break;
+        default:
+            break;
+    }
+
+    switch (Get_Fog_Func()) {
+        case FOG_DISABLE:
+            desc += "FOG_DISABLE | ";
+            break;
+        case FOG_ENABLE:
+            desc += "FOG_ENABLE | ";
+            break;
+        case FOG_SCALE_FRAGMENT:
+            desc += "FOG_SCALE_FRAGMENT | ";
+            break;
+        case FOG_WHITE:
+            desc += "FOG_WHITE | ";
+            break;
+        default:
+            break;
+    }
+
+    switch (Get_Primary_Gradient()) {
+        case GRADIENT_DISABLE:
+            desc += "GRADIENT_DISABLE | ";
+            break;
+        case GRADIENT_MODULATE:
+            desc += "GRADIENT_MODULATE | ";
+            break;
+        case GRADIENT_ADD:
+            desc += "GRADIENT_ADD | ";
+            break;
+        case GRADIENT_BUMPENVMAP:
+            desc += "GRADIENT_BUMPENVMAP | ";
+            break;
+        case GRADIENT_BUMPENVMAPLUMINANCE:
+            desc += "GRADIENT_BUMPENVMAPLUMINANCE | ";
+            break;
+        case GRADIENT_MODULATE2X:
+            desc += "GRADIENT_MODULATE2X | ";
+            break;
+        default:
+            break;
+    }
+
+    switch (Get_Secondary_Gradient()) {
+        case SECONDARY_GRADIENT_DISABLE:
+            desc += "SECONDARY_GRADIENT_DISABLE | ";
+            break;
+        case SECONDARY_GRADIENT_ENABLE:
+            desc += "SECONDARY_GRADIENT_ENABLE | ";
+            break;
+        default:
+            break;
+    }
+
+    switch (Get_Src_Blend_Func()) {
+        case SRCBLEND_ZERO:
+            desc += "SRCBLEND_ZERO | ";
+            break;
+        case SRCBLEND_ONE:
+            desc += "SRCBLEND_ONE | ";
+            break;
+        case SRCBLEND_SRC_ALPHA:
+            desc += "SRCBLEND_SRC_ALPHA | ";
+            break;
+        case SRCBLEND_ONE_MINUS_SRC_ALPHA:
+            desc += "SRCBLEND_ONE_MINUS_SRC_ALPHA | ";
+            break;
+        default:
+            break;
+    }
+
+    switch (Get_Texturing()) {
+        case TEXTURING_DISABLE:
+            desc += "TEXTURING_DISABLE | ";
+            break;
+        case TEXTURING_ENABLE:
+            desc += "TEXTURING_ENABLE | ";
+            break;
+        default:
+            break;
+    }
+
+    switch (Get_NPatch_Enable()) {
+        case NPATCH_DISABLE:
+            desc += "NPATCH_DISABLE | ";
+            break;
+        case NPATCH_ENABLE:
+            desc += "NPATCH_ENABLE | ";
+            break;
+        default:
+            break;
+    }
+
+    switch (Get_Alpha_Test()) {
+        case ALPHATEST_DISABLE:
+            desc += "ALPHATEST_DISABLE | ";
+            break;
+        case ALPHATEST_ENABLE:
+            desc += "ALPHATEST_ENABLE | ";
+            break;
+        default:
+            break;
+    }
+
+    switch (Get_Cull_Mode()) {
+        case CULL_MODE_DISABLE:
+            desc += "CULL_MODE_DISABLE | ";
+            break;
+        case CULL_MODE_ENABLE:
+            desc += "CULL_MODE_ENABLE | ";
+            break;
+        default:
+            break;
+    }
+
+    switch (Get_Post_Detail_Color_Func()) {
+        case DETAILCOLOR_DISABLE:
+            desc += "DETAILCOLOR_DISABLE";
+            break;
+        case DETAILCOLOR_DETAIL:
+            desc += "DETAILCOLOR_DETAIL";
+            break;
+        case DETAILCOLOR_SCALE:
+            desc += "DETAILCOLOR_SCALE";
+            break;
+        case DETAILCOLOR_INVSCALE:
+            desc += "DETAILCOLOR_INVSCALE";
+            break;
+        case DETAILCOLOR_ADD:
+            desc += "DETAILCOLOR_ADD";
+            break;
+        case DETAILCOLOR_SUB:
+            desc += "DETAILCOLOR_SUB";
+            break;
+        case DETAILCOLOR_SUBR:
+            desc += "DETAILCOLOR_SUBR";
+            break;
+        case DETAILCOLOR_BLEND:
+            desc += "DETAILCOLOR_BLEND";
+            break;
+        case DETAILCOLOR_DETAILBLEND:
+            desc += "DETAILCOLOR_DETAILBLEND";
+            break;
+        case DETAILCOLOR_ADDSIGNED:
+            desc += "DETAILCOLOR_ADDSIGNED";
+            break;
+        case DETAILCOLOR_ADDSIGNED2X:
+            desc += "DETAILCOLOR_ADDSIGNED2X";
+            break;
+        case DETAILCOLOR_SCALE2X:
+            desc += "DETAILCOLOR_SCALE2X";
+            break;
+        case DETAILCOLOR_MODALPHAADDCOLOR:
+            desc += "DETAILCOLOR_MODALPHAADDCOLOR";
+            break;
+        default:
+            return;
+    }
+}
+
+void ShaderClass::Invert_Backface_Culling(bool onoff)
+{
+    Invalidate();
+    s_polygonCullMode = onoff ? 3 : 2;
+}
+
+void ShaderClass::Report_Unable_To_Fog(const char *source)
+{
+    DEBUG_LOG("WARNING: Unable to fog shader in %s with given blending mode.\n");
+}
+
+void ShaderClass::Apply()
+{
+    // TODO Needs DX8Wrapper, DX8Caps.
+#ifndef THYME_STANDALONE
+    Call_Method<void, ShaderClass>(0x00813590, this);
+#endif
+}
+
---
+++ src/w3d/renderer/shader.h
@@ -0,0 +1,464 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ * @author tomsons26
+ * @author Tiberium Technologies
+ *
+ * @brief Base class for objects that use shaders.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#include "always.h"
+
+class StringClass;
+struct W3dMaterial3Struct;
+
+enum ShaderShiftConstants
+{
+    SHIFT_DEPTHCOMPARE = 0,
+    SHIFT_DEPTHMASK = 3,
+    SHIFT_COLORMASK = 4,
+    SHIFT_DSTBLEND = 5,
+    SHIFT_FOG = 8,
+    SHIFT_PRIGRADIENT = 10,
+    SHIFT_SECGRADIENT = 13,
+    SHIFT_SRCBLEND = 14,
+    SHIFT_TEXTURING = 16,
+    SHIFT_NPATCHENABLE = 17,
+    SHIFT_ALPHATEST = 18,
+    SHIFT_CULLMODE = 19,
+    SHIFT_POSTDETAILCOLORFUNC = 20,
+    SHIFT_POSTDETAILALPHAFUNC = 24,
+};
+
+// Utility function to convert enums to bitfield. Based on a macro provided by TT team.
+inline constexpr uint32_t Shader_Const(uint32_t depth_compare, uint32_t depth_mask, uint32_t color_mask, uint32_t src_blend,
+    uint32_t dst_blend, uint32_t fog, uint32_t pri_grad, uint32_t sec_grad, uint32_t texture, uint32_t alpha_test,
+    uint32_t cullmode, uint32_t post_det_color, uint32_t post_det_alpha)
+{
+    return depth_compare << SHIFT_DEPTHCOMPARE | depth_mask << SHIFT_DEPTHMASK | color_mask << SHIFT_COLORMASK
+        | dst_blend << SHIFT_DSTBLEND | fog << SHIFT_FOG | pri_grad << SHIFT_PRIGRADIENT | sec_grad << SHIFT_SECGRADIENT
+        | src_blend << SHIFT_SRCBLEND | texture << SHIFT_TEXTURING | alpha_test << SHIFT_ALPHATEST
+        | cullmode << SHIFT_CULLMODE | post_det_color << SHIFT_POSTDETAILCOLORFUNC
+        | post_det_alpha << SHIFT_POSTDETAILALPHAFUNC;
+}
+
+class ShaderClass
+{
+    friend class DX8Wrapper;
+
+    // Some internal constants for the guess sort level function.
+    // TODO, move somewhere else if required.
+    enum
+    {
+        SORT_LEVEL_NONE = 0,
+        SORT_LEVEL_BIN1 = 20,
+        SORT_LEVEL_BIN2 = 15,
+        SORT_LEVEL_BIN3 = 10,
+        MAX_SORT_LEVEL = 32,
+    };
+
+public:
+    enum AlphaTestType
+    {
+        ALPHATEST_DISABLE,
+        ALPHATEST_ENABLE,
+        ALPHATEST_MAX
+    };
+
+    enum DepthCompareType
+    {
+        PASS_NEVER,
+        PASS_LESS,
+        PASS_EQUAL,
+        PASS_LEQUAL,
+        PASS_GREATER,
+        PASS_NOTEQUAL,
+        PASS_GEQUAL,
+        PASS_ALWAYS,
+        PASS_MAX
+    };
+
+    enum DepthMaskType
+    {
+        DEPTH_WRITE_DISABLE,
+        DEPTH_WRITE_ENABLE,
+        DEPTH_WRITE_MAX
+    };
+
+    enum ColorMaskType
+    {
+        COLOR_WRITE_DISABLE,
+        COLOR_WRITE_ENABLE,
+        COLOR_WRITE_MAX
+    };
+
+    enum DetailAlphaFuncType
+    {
+        DETAILALPHA_DISABLE,
+        DETAILALPHA_DETAIL,
+        DETAILALPHA_SCALE,
+        DETAILALPHA_INVSCALE,
+        DETAILALPHA_MAX
+    };
+
+    enum DetailColorFuncType
+    {
+        DETAILCOLOR_DISABLE,
+        DETAILCOLOR_DETAIL,
+        DETAILCOLOR_SCALE,
+        DETAILCOLOR_INVSCALE,
+        DETAILCOLOR_ADD,
+        DETAILCOLOR_SUB,
+        DETAILCOLOR_SUBR,
+        DETAILCOLOR_BLEND,
+        DETAILCOLOR_DETAILBLEND,
+        DETAILCOLOR_ADDSIGNED, // following are new in ZH
+        DETAILCOLOR_ADDSIGNED2X,
+        DETAILCOLOR_SCALE2X,
+        DETAILCOLOR_MODALPHAADDCOLOR,
+        DETAILCOLOR_MAX
+    };
+
+    enum CullModeType
+    {
+        CULL_MODE_DISABLE,
+        CULL_MODE_ENABLE,
+        CULL_MODE_MAX
+    };
+
+    enum NPatchEnableType
+    {
+        NPATCH_DISABLE,
+        NPATCH_ENABLE,
+        NPATCH_TYPE_MAX
+    };
+
+    enum DstBlendFuncType
+    {
+        DSTBLEND_ZERO,
+        DSTBLEND_ONE,
+        DSTBLEND_SRC_COLOR,
+        DSTBLEND_ONE_MINUS_SRC_COLOR,
+        DSTBLEND_SRC_ALPHA,
+        DSTBLEND_ONE_MINUS_SRC_ALPHA,
+        DSTBLEND_MAX
+    };
+
+    enum FogFuncType
+    {
+        FOG_DISABLE,
+        FOG_ENABLE,
+        FOG_SCALE_FRAGMENT,
+        FOG_WHITE,
+        FOG_MAX
+    };
+
+    enum PriGradientType
+    {
+        GRADIENT_DISABLE,
+        GRADIENT_MODULATE,
+        GRADIENT_ADD,
+        GRADIENT_BUMPENVMAP,
+        GRADIENT_BUMPENVMAPLUMINANCE,
+        GRADIENT_MODULATE2X, // new in ZH
+        GRADIENT_MAX
+    };
+
+    enum SecGradientType
+    {
+        SECONDARY_GRADIENT_DISABLE,
+        SECONDARY_GRADIENT_ENABLE,
+        SECONDARY_GRADIENT_MAX
+    };
+
+    enum SrcBlendFuncType
+    {
+        SRCBLEND_ZERO,
+        SRCBLEND_ONE,
+        SRCBLEND_SRC_ALPHA,
+        SRCBLEND_ONE_MINUS_SRC_ALPHA,
+        SRCBLEND_MAX
+    };
+
+    enum TexturingType
+    {
+        TEXTURING_DISABLE,
+        TEXTURING_ENABLE,
+        TEXTURING_MAX
+    };
+
+    enum StaticSortCategoryType
+    {
+        SSCAT_OPAQUE,
+        SSCAT_ALPHA_TEST,
+        SSCAT_ADDITIVE,
+        SSCAT_OTHER,
+        SSCAT_OTHER2,
+    };
+
+    // Masks for where various shader options are held within the bitfield.
+    // clang-format off
+    enum
+    {
+        MASK_DEPTHCOMPARE = (7 << 0),          // 0x00000007 -  0 00000000000000000000000000000111
+        MASK_DEPTHMASK = (1 << 3),             // 0x00000008 -  3 00000000000000000000000000001000
+        MASK_COLORMASK = (1 << 4),             // 0x00000010 -  4 00000000000000000000000000010000
+        MASK_DSTBLEND = (7 << 5),              // 0x000000E0 -  5 00000000000000000000000011100000
+        MASK_FOG = (3 << 8),                   // 0x00000300 -  8 00000000000000000000001100000000
+        MASK_PRIGRADIENT = (7 << 10),          // 0x00001C00 - 10 00000000000000000001110000000000
+        MASK_SECGRADIENT = (1 << 13),          // 0x00002000 - 13 00000000000000000010000000000000
+        MASK_SRCBLEND = (3 << 14),             // 0x0000C000 - 14 00000000000000001100000000000000
+        MASK_TEXTURING = (1 << 16),            // 0x00010000 - 16 00000000000000010000000000000000
+        MASK_NPATCHENABLE = (1 << 17),         // 0x00020000 - 17 00000000000000100000000000000000
+        MASK_ALPHATEST = (1 << 18),            // 0x00040000 - 18 00000000000001000000000000000000
+        MASK_CULLMODE = (1 << 19),             // 0x00080000 - 19 00000000000010000000000000000000
+        MASK_POSTDETAILCOLORFUNC = (15 << 20), // 0x00F00000 - 20 00000000111100000000000000000000
+        MASK_POSTDETAILALPHAFUNC = (7 << 24),  // 0x07000000 - 24 00000111000000000000000000000000
+    };
+    // clang-format on
+
+public:
+    ShaderClass() { Reset(); }
+    ShaderClass(const ShaderClass &that) : m_shaderBits(that.m_shaderBits) {}
+    ShaderClass(const uint32_t bits) : m_shaderBits(bits) {}
+
+    bool operator==(const ShaderClass &that) { return m_shaderBits == that.m_shaderBits; }
+    bool operator!=(const ShaderClass &that) { return m_shaderBits != that.m_shaderBits; }
+
+    bool Uses_Alpha() const;
+    bool Uses_Fog() const { return Get_Fog_Func() != FOG_DISABLE; }
+    bool Uses_Primary_Gradient() const { return Get_Primary_Gradient() != GRADIENT_DISABLE; }
+    bool Uses_Secondary_Gradient() const { return Get_Secondary_Gradient() != SECONDARY_GRADIENT_DISABLE; }
+    bool Uses_Texture() const { return Get_Texturing() != TEXTURING_DISABLE; }
+    bool Uses_Post_Detail_Texture() const;
+    void Reset() { m_shaderBits = 0x8441B; } // TODO decode to Shader_Const call.
+    void Init_From_Material3(const W3dMaterial3Struct &material);
+    void Enable_Fog(const char *source);
+    StaticSortCategoryType Get_Static_Sort_Category() const;
+    int Guess_Sort_Level() const;
+    void Get_Description(StringClass &desc); // new function in ZH
+
+    uint32_t Get_Bits() const { return m_shaderBits; }
+
+    DepthCompareType Get_Depth_Compare() const
+    {
+        return (DepthCompareType)(m_shaderBits & MASK_DEPTHCOMPARE >> SHIFT_DEPTHCOMPARE);
+    }
+
+    DepthMaskType Get_Depth_Mask() const { return (DepthMaskType)((m_shaderBits & MASK_DEPTHMASK) >> SHIFT_DEPTHMASK); }
+
+    ColorMaskType Get_Color_Mask() const { return (ColorMaskType)((m_shaderBits & MASK_COLORMASK) >> SHIFT_COLORMASK); }
+
+    DetailAlphaFuncType Get_Post_Detail_Alpha_Func() const
+    {
+        return (DetailAlphaFuncType)((m_shaderBits & MASK_POSTDETAILALPHAFUNC) >> SHIFT_POSTDETAILALPHAFUNC);
+    }
+
+    DetailColorFuncType Get_Post_Detail_Color_Func() const
+    {
+        return (DetailColorFuncType)((m_shaderBits & MASK_POSTDETAILCOLORFUNC) >> SHIFT_POSTDETAILCOLORFUNC);
+    }
+
+    AlphaTestType Get_Alpha_Test() const { return (AlphaTestType)((m_shaderBits & MASK_ALPHATEST) >> SHIFT_ALPHATEST); }
+
+    CullModeType Get_Cull_Mode() const { return (CullModeType)((m_shaderBits & MASK_CULLMODE) >> SHIFT_CULLMODE); }
+
+    DstBlendFuncType Get_Dst_Blend_Func() const
+    {
+        return (DstBlendFuncType)((m_shaderBits & MASK_DSTBLEND) >> SHIFT_DSTBLEND);
+    }
+
+    FogFuncType Get_Fog_Func() const { return (FogFuncType)((m_shaderBits & MASK_FOG) >> SHIFT_FOG); }
+
+    PriGradientType Get_Primary_Gradient() const
+    {
+        return (PriGradientType)((m_shaderBits & MASK_PRIGRADIENT) >> SHIFT_PRIGRADIENT);
+    }
+
+    SecGradientType Get_Secondary_Gradient() const
+    {
+        return (SecGradientType)((m_shaderBits & MASK_SECGRADIENT) >> SHIFT_SECGRADIENT);
+    }
+
+    SrcBlendFuncType Get_Src_Blend_Func() const
+    {
+        return (SrcBlendFuncType)((m_shaderBits & MASK_SRCBLEND) >> SHIFT_SRCBLEND);
+    }
+
+    TexturingType Get_Texturing() const { return (TexturingType)((m_shaderBits & MASK_TEXTURING) >> SHIFT_TEXTURING); }
+
+    NPatchEnableType Get_NPatch_Enable() const
+    {
+        return (NPatchEnableType)((m_shaderBits & MASK_NPATCHENABLE) >> SHIFT_NPATCHENABLE);
+    }
+
+    void Set_Depth_Compare(DepthCompareType x)
+    {
+        m_shaderBits &= ~MASK_DEPTHCOMPARE;
+        m_shaderBits |= x << SHIFT_DEPTHCOMPARE;
+    }
+
+    void Set_Depth_Mask(DepthMaskType x)
+    {
+        m_shaderBits &= ~MASK_DEPTHMASK;
+        m_shaderBits |= x << SHIFT_DEPTHMASK;
+    }
+
+    void Set_Color_Mask(ColorMaskType x)
+    {
+        m_shaderBits &= ~MASK_COLORMASK;
+        m_shaderBits |= x << SHIFT_COLORMASK;
+    }
+
+    void Set_Post_Detail_Alpha_Func(DetailAlphaFuncType x)
+    {
+        m_shaderBits &= ~MASK_POSTDETAILALPHAFUNC;
+        m_shaderBits |= x << SHIFT_POSTDETAILALPHAFUNC;
+    }
+
+    void Set_Post_Detail_Color_Func(DetailColorFuncType x)
+    {
+        m_shaderBits &= ~MASK_POSTDETAILCOLORFUNC;
+        m_shaderBits |= x << SHIFT_POSTDETAILCOLORFUNC;
+    }
+
+    void Set_Alpha_Test(AlphaTestType x)
+    {
+        m_shaderBits &= ~MASK_ALPHATEST;
+        m_shaderBits |= x << SHIFT_ALPHATEST;
+    }
+
+    void Set_Cull_Mode(CullModeType x)
+    {
+        m_shaderBits &= ~MASK_CULLMODE;
+        m_shaderBits |= x << SHIFT_CULLMODE;
+    }
+
+    void Set_Dst_Blend_Func(DstBlendFuncType x)
+    {
+        m_shaderBits &= ~MASK_DSTBLEND;
+        m_shaderBits |= x << SHIFT_DSTBLEND;
+    }
+
+    void Set_Fog_Func(FogFuncType x)
+    {
+        m_shaderBits &= ~MASK_FOG;
+        m_shaderBits |= x << SHIFT_FOG;
+    }
+
+    void Set_Primary_Gradient(PriGradientType x)
+    {
+        m_shaderBits &= ~MASK_PRIGRADIENT;
+        m_shaderBits |= x << SHIFT_PRIGRADIENT;
+    }
+
+    void Set_Secondary_Gradient(SecGradientType x)
+    {
+        m_shaderBits &= ~MASK_SECGRADIENT;
+        m_shaderBits |= x << SHIFT_SECGRADIENT;
+    }
+
+    void Set_Src_Blend_Func(SrcBlendFuncType x)
+    {
+        m_shaderBits &= ~MASK_SRCBLEND;
+        m_shaderBits |= x << SHIFT_SRCBLEND;
+    }
+
+    void Set_Texturing(TexturingType x)
+    {
+        m_shaderBits &= ~MASK_TEXTURING;
+        m_shaderBits |= x << SHIFT_TEXTURING;
+    }
+
+    void Set_NPatch_Enable(NPatchEnableType x)
+    {
+        m_shaderBits &= ~MASK_NPATCHENABLE;
+        m_shaderBits |= x << SHIFT_NPATCHENABLE;
+    }
+
+    static void Invalidate() { s_shaderDirty = true; }
+    static void Invert_Backface_Culling(bool onoff);
+    static bool Is_Backface_Culling_Inverted() { return s_polygonCullMode == 3; }
+
+protected:
+    void Report_Unable_To_Fog(const char *source);
+
+private:
+    void Apply();
+
+public:
+    static ShaderClass s_presetOpaqueShader;
+    static ShaderClass s_presetAdditiveShader;
+    static ShaderClass s_presetBumpenvmapShader;
+    static ShaderClass s_presetAlphaShader;
+    static ShaderClass s_presetMultiplicativeShader;
+    static ShaderClass s_presetOpaque2DShader;
+    static ShaderClass s_presetOpaqueSpriteShader;
+    static ShaderClass s_presetAdditive2DShader;
+    static ShaderClass s_presetAlpha2DShader;
+    static ShaderClass s_presetAdditiveSpriteShader;
+    static ShaderClass s_presetAlphaSpriteShader;
+    static ShaderClass s_presetOpaqueSolidShader;
+    static ShaderClass s_presetAdditiveSolidShader;
+    static ShaderClass s_presetAlphaSolidShader;
+    static ShaderClass s_presetATest2DShader;
+    static ShaderClass s_presetATestSpriteShader;
+    static ShaderClass s_presetATestBlend2DShader;
+    static ShaderClass s_presetATestBlendSpriteShader;
+    static ShaderClass s_presetScreen2DShader;
+    static ShaderClass s_presetScreenSpriteShader;
+    static ShaderClass s_presetMultiplicative2DShader;
+    static ShaderClass s_presetMultiplicativeSpriteShader;
+
+protected:
+    uint32_t m_shaderBits;
+#ifndef THYME_STANDALONE
+    static uint32_t &s_currentShader;
+    static bool &s_shaderDirty;
+    static uint32_t &s_polygonCullMode;
+#else
+    static uint32_t s_currentShader;
+    static bool s_shaderDirty;
+    static uint32_t s_polygonCullMode;
+#endif
+};
+
+inline bool ShaderClass::Uses_Alpha() const
+{
+    // check if alpha test is enabled
+    if (Get_Alpha_Test() != ALPHATEST_DISABLE) {
+        return true;
+    }
+
+    DstBlendFuncType dst = Get_Dst_Blend_Func();
+
+    if (dst == DSTBLEND_SRC_ALPHA || dst == DSTBLEND_ONE_MINUS_SRC_ALPHA) {
+        return true;
+    }
+
+    SrcBlendFuncType src = Get_Src_Blend_Func();
+
+    return src == SRCBLEND_SRC_ALPHA || src == SRCBLEND_ONE_MINUS_SRC_ALPHA;
+}
+
+inline bool ShaderClass::Uses_Post_Detail_Texture() const
+{
+    if (Get_Texturing() == TEXTURING_DISABLE) {
+        return false;
+    }
+
+    return (Get_Post_Detail_Color_Func() != DETAILCOLOR_DISABLE) || (Get_Post_Detail_Alpha_Func() != DETAILALPHA_DISABLE);
+}
+
+#ifndef THYME_STANDALONE
+#include "hooker.h"
+#endif

c9aa1c79dc337f5a6dac301e4ce057f21b84f8d3
OmniBlade <nadia_xy@hotmail.com>
2018-11-27 15:59:01 +0000

    Refactors bittype.h to use compiler attribute detection.

--- src/base/compiler.h
+++ src/base/compiler.h
@@ -1,30 +1,23 @@
-////////////////////////////////////////////////////////////////////////////////
-//                               --  THYME  --                                //
-////////////////////////////////////////////////////////////////////////////////
-//
-//  Project Name:: Thyme
-//
-//          File:: COMPILER.H
-//
-//        Author:: Paul Pedriana (see below)
-//
-//  Contributors:: CCHyper & OmniBlade
-//
-//   Description:: 
-//
-//         Notes:: This is a heavily modified version of the EACOMPLIER.H file
-//                 from the EASTL (Electronic Arts Standard Type Library),
-//                 written by Paul Pedriana of Maxis. Source: http://gpl.ea.com/
-//
-//       License:: Thyme is free software: you can redistribute it and/or 
-//                 modify it under the terms of the GNU General Public License 
-//                 as published by the Free Software Foundation, either version 
-//                 2 of the License, or (at your option) any later version.
-//
-//                 A full copy of the GNU General Public License can be found in
-//                 LICENSE
-//
-////////////////////////////////////////////////////////////////////////////////
+/**
+ * @file
+ *
+ * @author Paul Pedriana (see below)
+ * @author CCHyper
+ * @author OmniBlade
+ *
+ * @brief Basic header files and defines that are always needed.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ *
+ * This is a heavily modified version of the EACOMPLIER.H file from the EASTL
+ * (Electronic Arts Standard Type Library), written by Paul Pedriana of Maxis.
+ * Source: http://gpl.ea.com/
+ */
 #pragma once
 
 /*

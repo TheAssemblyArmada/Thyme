c9f646b5fa1282f030ad056ede5e12fac81ecd92
OmniBlade <nadia_xy@hotmail.com>
2018-11-27 15:59:01 +0000

    Refactors bittype.h to use compiler attribute detection.

--- src/base/bittype.h
+++ src/base/bittype.h
@@ -1,12 +1,10 @@
 /**
  * @file
  *
+ * @author CCHyper
  * @author OmniBlade
  *
- * @brief Fixed width types.
- *
- * Provides standard definitions for fixed width integers on all supported platforms and types to use for type punning
- * pointers.
+ * @brief Includes for defined width integers and aliasing versions for type punning.
  *
  * @copyright Thyme is free software: you can redistribute it and/or
  *            modify it under the terms of the GNU General Public License
@@ -17,13 +15,9 @@
  */
 #pragma once
 
-// We aren't supporting MSVC versions earlier than 2010
-#if defined(__GNUC__) || defined(__clang__) || defined(__WATCOM__) || _MSC_VER >= 1600 // GNU C++ or clang
+#include "macros.h"
+#include <inttypes.h>
 #include <limits.h>
-#include <stdint.h>
-#else // !_MSC_VER || !(__GNUC__ || __clang__ || __WATCOM__)
-#error Unknown compiler.Please specify fixed - size integer types in 'bittype.h'.
-#endif // _MSC_VER || (__GNUC__ || __clang__ || __WATCOM__)
 
 #ifdef THYME_USE_ICU
 #include <unicode/uchar.h>
@@ -32,27 +26,27 @@ typedef UChar unichar_t;
 typedef wchar_t unichar_t;
 #endif
 
-// Use these as pointers to do type punning.
-#if defined(__GNUC__) || defined(__clang__)
-typedef float __attribute__((__may_alias__)) float_a;
-typedef int32_t __attribute__((__may_alias__)) int32_a;
-typedef uint32_t __attribute__((__may_alias__)) uint32_a;
-typedef double __attribute__((__may_alias__)) double_a;
-typedef int64_t __attribute__((__may_alias__)) int64_a;
-typedef uint64_t __attribute__((__may_alias__)) uint64_a;
-typedef unichar_t __attribute__((__may_alias__)) unichar_a;
+// Use these when using pointers to do type punning.
+#if __has_attribute(__may_alias__)
+    typedef float __attribute__((__may_alias__)) float_a;
+    typedef int32_t __attribute__((__may_alias__)) int32_a;
+    typedef uint32_t __attribute__((__may_alias__)) uint32_a;
+    typedef double __attribute__((__may_alias__)) double_a;
+    typedef int64_t __attribute__((__may_alias__)) int64_a;
+    typedef uint64_t __attribute__((__may_alias__)) uint64_a;
+    typedef unichar_t __attribute__((__may_alias__)) unichar_a;
 #else
-// MSVC doesn't currently enforce strict aliasing.
-typedef float float_a;
-typedef int32_t int32_a;
-typedef uint32_t uint32_a;
-typedef double double_a;
-typedef int64_t int64_a;
-typedef uint64_t uint64_a;
-typedef unichar_t unichar_a;
+    // MSVC doesn't currently enforce strict aliasing.
+    typedef float float_a;
+    typedef int32_t int32_a;
+    typedef uint32_t uint32_a;
+    typedef double double_a;
+    typedef int64_t int64_a;
+    typedef uint64_t uint64_a;
+    typedef unichar_t unichar_a;
 #endif
 
-// Union to use for type punning.
+// Union to use for type punning floats.
 union float_int_tp
 {
     uint32_a integer;

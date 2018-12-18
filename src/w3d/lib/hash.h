77aef417ce9ded1e03435b26698602b41522fc98
Toms <tomsons26lv@gmail.com>
2018-12-18 03:09:01 +0200

    add hash code that will be needed later

---
+++ src/w3d/lib/hash.h
@@ -0,0 +1,66 @@
+/**
+ * @file
+ *
+ * @author tomsons26
+ *
+ * @brief Classes for storing hashes in tables.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+#include "always.h"
+
+class HashableClass
+{
+    friend class HashTableClass;
+    friend class HashTableIteratorClass;
+public:
+    HashableClass() : m_nextHash(nullptr) {}
+    virtual ~HashableClass() {}
+    virtual const char *Get_Key() = 0;
+
+private:
+    HashableClass *m_nextHash;
+};
+
+class HashTableClass
+{
+    friend class HashTableIteratorClass;
+public:
+    HashTableClass(int size);
+    ~HashTableClass();
+    void Reset();
+    void Add(HashableClass *entry);
+    bool Remove(HashableClass *entry);
+    HashableClass *Find(char const *key);
+private:
+    int Hash(const char *key);
+
+private:
+    int m_hashTableSize;
+    HashableClass **m_hashTable;
+};
+
+class HashTableIteratorClass
+{
+public:
+    HashTableIteratorClass(HashTableClass &table) : m_table(table) {}
+    virtual ~HashTableIteratorClass() {}
+    void First();
+    void Next();
+    bool Is_Done() { return m_currentEntry == nullptr; } // i think, based on enb..
+    HashableClass *Get_Current() { return m_currentEntry; }
+private:
+    void Advance_Next();
+
+private:
+    HashTableClass &m_table;
+    int m_index;
+    HashableClass *m_currentEntry;
+    HashableClass *m_nextEntry;
+};

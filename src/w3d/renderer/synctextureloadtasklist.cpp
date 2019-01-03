72cec5ed7a6066e600f2d9e04ef6ec4342fbcf39
OmniBlade <nadia_xy@hotmail.com>
2019-01-03 13:26:50 +0000

    Implements SynchronizedTextureLoadTaskListClass.

---
+++ src/w3d/renderer/synctextureloadtasklist.cpp
@@ -0,0 +1,54 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ * @author Tiberian Technologies
+ *
+ * @brief Class for holding a list of texture loading tasks with syncronization.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "synctextureloadtasklist.h"
+
+#ifndef THYME_STANDALONE
+SynchronizedTextureLoadTaskListClass &g_foregroundQueue = Make_Global<SynchronizedTextureLoadTaskListClass>(0x00A4C610);
+SynchronizedTextureLoadTaskListClass &g_backgroundQueue = Make_Global<SynchronizedTextureLoadTaskListClass>(0x00A4C678);
+#else
+SynchronizedTextureLoadTaskListClass g_foregroundQueue;
+SynchronizedTextureLoadTaskListClass g_backgroundQueue;
+#endif
+
+void SynchronizedTextureLoadTaskListClass::Push_Front(TextureLoadTaskClass *task)
+{
+    FastCriticalSectionClass::LockClass lock(m_mutex);
+    TextureLoadTaskListClass::Push_Front(task);
+}
+
+void SynchronizedTextureLoadTaskListClass::Push_Back(TextureLoadTaskClass *task)
+{
+    FastCriticalSectionClass::LockClass lock(m_mutex);
+    TextureLoadTaskListClass::Push_Back(task);
+}
+
+TextureLoadTaskClass *SynchronizedTextureLoadTaskListClass::Pop_Front()
+{
+    FastCriticalSectionClass::LockClass lock(m_mutex);
+    return TextureLoadTaskListClass::Pop_Front();
+}
+
+TextureLoadTaskClass *SynchronizedTextureLoadTaskListClass::Pop_Back()
+{
+    FastCriticalSectionClass::LockClass lock(m_mutex);
+    return TextureLoadTaskListClass::Pop_Back();
+}
+
+void SynchronizedTextureLoadTaskListClass::Remove(TextureLoadTaskClass *task)
+{
+    FastCriticalSectionClass::LockClass lock(m_mutex);
+    TextureLoadTaskListClass::Remove(task);
+}
---
+++ src/w3d/renderer/synctextureloadtasklist.h
@@ -0,0 +1,43 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ * @author Tiberian Technologies
+ *
+ * @brief Class for holding a list of texture loading tasks with syncronization.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#include "always.h"
+#include "critsection.h"
+#include "textureloadtasklist.h"
+
+class SynchronizedTextureLoadTaskListClass : public TextureLoadTaskListClass
+{
+public:
+    void Push_Front(TextureLoadTaskClass *task);
+    void Push_Back(TextureLoadTaskClass *task);
+    TextureLoadTaskClass *Pop_Front();
+    TextureLoadTaskClass *Pop_Back();
+    void Remove(TextureLoadTaskClass *task);
+
+private:
+    bool m_unused;
+    FastCriticalSectionClass m_mutex;
+};
+
+#ifndef THYME_STANDALONE
+#include "hooker.h"
+extern SynchronizedTextureLoadTaskListClass &g_foregroundQueue;
+extern SynchronizedTextureLoadTaskListClass &g_backgroundQueue;
+#else
+extern SynchronizedTextureLoadTaskListClass g_foregroundQueue;
+extern SynchronizedTextureLoadTaskListClass g_backgroundQueue;
+#endif

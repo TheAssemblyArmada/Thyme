1e0f82413975959c4224a5be6fff8ac1ac5ac5d4
OmniBlade <nadia_xy@hotmail.com>
2019-01-04 13:36:22 +0000

    Performs some cleanup on the HAnim classes.
    Previous merge was premature.

--- src/w3d/renderer/hanimmgr.h
+++ src/w3d/renderer/hanimmgr.h
@@ -1,38 +1,44 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ * @author tomsons26
+ *
+ * @brief Hashed animation manager.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
 #pragma once
 
+#include "always.h"
 #include "chunkio.h"
 #include "hash.h"
+#include "missing.h"
+#include "vector.h"
 #include "wwstring.h"
 
 class W3DExclusionListClass;
-class DynamicVectorClass;
 class HAnimClass;
-
-class MissingAnimClass : public HashableClass
-{
-public:
-    MissingAnimClass(const char *name) : Name(name) {}
-    MissingAnimClass(MissingAnimClass &){}; // unknown if it just sets name or hash table too
-    virtual ~MissingAnimClass() { m_name.Free_String(); };
-    virtual const char *Get_Key() override { return m_name.Peek_Buffer(); };
-
-private:
-    StringClass m_name;
-};
 
 class HAnimManagerClass
 {
 public:
     HAnimManagerClass();
     ~HAnimManagerClass();
+
     int Load_Anim(ChunkLoadClass &cload);
     int Load_Morph_Anim(ChunkLoadClass &cload);
     int Load_Raw_Anim(ChunkLoadClass &cload);
     int Load_Compressed_Anim(ChunkLoadClass &cload);
     HAnimClass *Get_Anim(const char *name);
     void Free_All_Anims();
-    void Free_All_Anims_With_Exclusion_List(W3DExclusionListClass const &);
-    void Create_Asset_List(DynamicVectorClass<StringClass> &);
+    void Free_All_Anims_With_Exclusion_List(const W3DExclusionListClass &exclude_list);
+    void Create_Asset_List(DynamicVectorClass<StringClass> &list);
     unsigned char Add_Anim(HAnimClass *new_anim);
     void Register_Missing(const char *name);
     unsigned char Is_Missing(const char *name);
@@ -40,21 +46,22 @@ public:
     HAnimClass *Peek_Anim(const char *name);
 
 private:
-    HashTableClass *AnimPtrTable;
-    HashTableClass *MissingAnimTable;
+    HashTableClass *m_animPtrTable;
+    HashTableClass *m_missingAnimTable;
 };
 
 class HAnimManagerIterator : public HashTableIteratorClass
 {
 public:
     HAnimManagerIterator(HAnimManagerIterator &iterator) : HashTableIteratorClass(iterator) {}
-    HAnimManagerIterator(HAnimManagerClass &animmanager) {} // code unknown currently
     virtual ~HAnimManagerIterator() {}
+
     HAnimClass *Get_Current_Anim()
     {
-        if (Get_Current()) {
-            return (HAnimClass *)Get_Current();//i think..
+        if (Get_Current() != nullptr) {
+            return (HAnimClass *)Get_Current();
         }
-        return 0;
+
+        return nullptr;
     }
 };

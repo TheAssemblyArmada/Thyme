27736e2f4dda0be3975617460782f78c63a965b0
OmniBlade <nadia_xy@hotmail.com>
2019-06-06 13:32:38 +0100

    Update file headers for audiomisc.

--- src/game/common/audio/audiomisc.cpp
+++ src/game/common/audio/audiomisc.cpp
@@ -1,26 +1,17 @@
-////////////////////////////////////////////////////////////////////////////////
-//                               --  THYME  --                                //
-////////////////////////////////////////////////////////////////////////////////
-//
-//  Project Name:: Thyme
-//
-//          File:: AUDIOMISC.CPP
-//
-//        Author:: OmniBlade
-//
-//  Contributors::
-//
-//   Description:: Collection of miscellaneous audio events.
-//
-//       License:: Thyme is free software: you can redistribute it and/or
-//                 modify it under the terms of the GNU General Public License
-//                 as published by the Free Software Foundation, either version
-//                 2 of the License, or (at your option) any later version.
-//
-//                 A full copy of the GNU General Public License can be found in
-//                 LICENSE
-//
-////////////////////////////////////////////////////////////////////////////////
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Class holding a collection of miscellaneous audio events.
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
 #include "audiomisc.h"
 #include "audiomanager.h"
 #include <cstddef>

aa41164759dcaf6a4242783d023acefc56f25899
Jonathan Wilson <jfwfreo@tpgi.com.au>
2020-05-11 18:15:54 +1000

    Move Vector2i to its own file where it belongs. Some fixes to Vector2

--- src/w3d/math/vector2.h
+++ src/w3d/math/vector2.h
@@ -19,38 +19,24 @@
 #include "gamemath.h"
 #include <captainslog.h>
 
-class Vector2i
-{
-public:
-    Vector2i() {}
-
-    Vector2i(int i, int j)
-    {
-        I = i;
-        J = j;
-    }
-
-    void Set(int i, int j)
-    {
-        I = i;
-        J = j;
-    }
-
-public:
-    int I;
-    int J;
-};
-
+__forceinline float Quick_Distance(float x1, float y1, float x2, float y2);
 class Vector2
 {
 public:
-    __forceinline Vector2(float x, float y) : X(x), Y(y) {}
-    __forceinline Vector2(const Vector2 &v) : X(v.X), Y(v.Y) {}
-    __forceinline Vector2() : X(0.0f), Y(0.0f) {}
-
-    __forceinline explicit Vector2(const float vector[2])
+    __forceinline Vector2(float x, float y)
     {
-        captainslog_assert(vector != nullptr);
+        X = x;
+        Y = y;
+    }
+    __forceinline Vector2(const Vector2 &v)
+    {
+        X = v.X;
+        Y = v.Y;
+    }
+    __forceinline Vector2() {}
+
+    __forceinline Vector2(const float vector[2])
+    {
         X = vector[0];
         Y = vector[1];
     }
@@ -81,15 +67,17 @@ public:
 
     __forceinline void Normalize()
     {
-        float len2 = GAMEMATH_FLOAT_TINY + Length2();
-        float oolen = GameMath::Inv_Sqrt(len2);
-        X *= oolen;
-        Y *= oolen;
+        float len2 = Length2();
+        if (len2 != 0.0f) {
+            float oolen = GameMath::Inv_Sqrt(len2);
+            X *= oolen;
+            Y *= oolen;
+        }
     }
 
     __forceinline float Length() const { return (float)GameMath::Sqrt(Length2()); }
 
-    __forceinline float Length2() const { return (float)(X * X + Y * Y); }
+    __forceinline float Length2() const { return (X * X + Y * Y); }
 
     __forceinline Vector2 operator-() const { return Vector2(-X, -Y); }
 
@@ -149,12 +137,13 @@ public:
 
     __forceinline bool Rotate_Towards_Vector(Vector2 &target, float max_s, float max_c, bool &positive_turn)
     {
-        positive_turn = Vector2::Perp_Dot_Product(target, *this) > 0.0f;
+        bool return_value = false;
+        positive_turn = Perp_Dot_Product(target, *this) > 0.0f;
 
         if (Vector2::Dot_Product(*this, target) >= max_c) {
             Set(target);
 
-            return true;
+            return_value = true;
         } else {
             if (positive_turn) {
                 Rotate(max_s, max_c);
@@ -163,7 +152,7 @@ public:
             }
         }
 
-        return false;
+        return return_value;
     }
 
     __forceinline void Update_Min(const Vector2 &a)
@@ -200,12 +189,6 @@ public:
         Y *= a.Y;
     }
 
-    __forceinline void Unscale(const Vector2 &a)
-    {
-        X /= a.X;
-        Y /= a.Y;
-    }
-
     __forceinline static float Distance(const Vector2 &p1, const Vector2 &p2)
     {
         Vector2 temp;
@@ -216,26 +199,25 @@ public:
 
     __forceinline static void Lerp(const Vector2 &a, const Vector2 &b, float t, Vector2 *set_result)
     {
+        captainslog_assert(set_result != nullptr);
         set_result->X = (a.X + (b.X - a.X) * t);
         set_result->Y = (a.Y + (b.Y - a.Y) * t);
     }
 
-    __forceinline void Floor()
+    __forceinline bool Is_Valid() const { return (GameMath::Is_Valid_Float(X) && GameMath::Is_Valid_Float(Y)); }
+    static __forceinline float Quick_Distance(const Vector2 &a, const Vector2 &b)
     {
-        X = GameMath::Floor(X);
-        Y = GameMath::Floor(Y);
-    };
-
+        return ::Quick_Distance(a.X, a.Y, b.X, b.Y);
+    }
     friend Vector2 operator*(const Vector2 &a, float k);
     friend Vector2 operator*(float k, const Vector2 &a);
     friend Vector2 operator/(const Vector2 &a, float k);
-    friend Vector2 operator/(const Vector2 &a, const Vector2 &b) { return Vector2(a.X / b.X, a.Y / b.Y); }
-    friend Vector2 operator/(const Vector2 &a, const Vector2i &b) { return Vector2(a.X / b.I, a.Y / b.J); }
     friend Vector2 operator+(const Vector2 &a, const Vector2 &b);
     friend Vector2 operator-(const Vector2 &a, const Vector2 &b);
     friend float operator*(const Vector2 &a, const Vector2 &b);
     friend bool operator==(const Vector2 &a, const Vector2 &b);
     friend bool operator!=(const Vector2 &a, const Vector2 &b);
+    friend bool Equal_Within_Epsilon(const Vector2 &a, const Vector2 &b, float epsilon);
 
 public:
     union
@@ -253,17 +235,24 @@ public:
 
 __forceinline Vector2 operator*(const Vector2 &a, float k)
 {
-    return Vector2(a[0] * k, a[1] * k);
+    float a0k(a[0] * k);
+    float a1k(a[1] * k);
+    return Vector2(a0k, a1k);
 }
 
 __forceinline Vector2 operator*(float k, const Vector2 &a)
 {
-    return Vector2(a[0] * k, a[1] * k);
+    float a0k(a[0] * k);
+    float a1k(a[1] * k);
+    return Vector2(a0k, a1k);
 }
 
 __forceinline Vector2 operator/(const Vector2 &a, float k)
 {
-    return Vector2(a[0] * (1.0f / k), a[1] * (1.0f / k));
+    float ook = 1.0f / k;
+    float a0ook(a[0] * ook);
+    float a1ook(a[1] * ook);
+    return Vector2(a0ook, a1ook);
 }
 
 __forceinline Vector2 operator+(const Vector2 &a, const Vector2 &b)
@@ -283,19 +272,28 @@ __forceinline float operator*(const Vector2 &a, const Vector2 &b)
 
 __forceinline bool operator==(const Vector2 &a, const Vector2 &b)
 {
-    return (a.X == b.X) | (a.Y == b.Y);
+    bool a0b0(a[0] == b[0]);
+    bool a1b1(a[1] == b[1]);
+    return (a0b0 & a1b1);
 }
 
 __forceinline bool operator!=(const Vector2 &a, const Vector2 &b)
 {
-    return (a.X != b.X) | (a.Y != b.Y);
+    bool a0b0(a[0] != b[0]);
+    bool a1b1(a[1] != b[1]);
+    return (a0b0 | a1b1);
 }
 
 __forceinline Vector2 Normalize(const Vector2 &vec)
 {
-    float len2 = GAMEMATH_FLOAT_TINY + vec.Length2();
+    float len2 = vec.Length2();
 
-    return vec * GameMath::Inv_Sqrt(len2);
+    if (len2 != 0.0f) {
+        float oolen = GameMath::Inv_Sqrt(len2);
+        return vec / oolen;
+    }
+
+    return Vector2(0.0f, 0.0f);
 }
 
 __forceinline void Swap(Vector2 &a, Vector2 &b)
@@ -305,10 +303,29 @@ __forceinline void Swap(Vector2 &a, Vector2 &b)
     b = tmp;
 }
 
+__forceinline float Quick_Distance(float x1, float y1, float x2, float y2)
+{
+    float x_diff = x1 - x2;
+    float y_diff = y1 - y2;
+    GameMath::Fabs(x_diff);
+    GameMath::Fabs(y_diff);
+
+    if (x_diff > y_diff) {
+        return ((y_diff / 2) + x_diff);
+    } else {
+        return ((x_diff / 2) + y_diff);
+    }
+}
+
 __forceinline float Distance(float x1, float y1, float x2, float y2)
 {
     float x_diff = x1 - x2;
     float y_diff = y1 - y2;
 
     return (GameMath::Sqrt((x_diff * x_diff) + (y_diff * y_diff)));
+}
+
+__forceinline bool Equal_Within_Epsilon(const Vector2 &a, const Vector2 &b, float epsilon)
+{
+    return ((GameMath::Fabs(a.X - b.X) < epsilon) && (GameMath::Fabs(a.Y - b.Y) < epsilon));
 }

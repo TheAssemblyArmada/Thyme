3e3ddf0d431368bdf6daf788b041379b6c2394dc
Jonathan Wilson <jfwfreo@tpgi.com.au>
2020-05-11 17:28:39 +1000

    Add some stuff to Quaternion that we do need and remove some stuff that we dont need.

--- src/w3d/math/quaternion.cpp
+++ src/w3d/math/quaternion.cpp
@@ -42,17 +42,29 @@ float Project_To_Sphere(float r, float x, float y)
 
 } // namespace
 
-const Quaternion Quaternion::IDENTITY(0.0f, 0.0f, 0.0f, 1.0f);
-
-Quaternion::Quaternion(float angle, const Vector3 &axis)
+Quaternion::Quaternion(const Vector3 &axis, float angle)
 {
-    float cosAngle = GameMath::Cos(angle / 2.f);
-    float sinAngle = GameMath::Sin(angle / 2.f);
+    float s = GameMath::Sin(angle / 2);
+    float c = GameMath::Cos(angle / 2);
+    X = s * axis.X;
+    Y = s * axis.Y;
+    Z = s * axis.Z;
+    W = c;
+}
 
-    t = cosAngle;
-    i = axis.X * sinAngle;
-    j = axis.Y * sinAngle;
-    k = axis.Z * sinAngle;
+void Quaternion::Rotate_X(float theta)
+{
+    *this = (*this) * Quaternion(Vector3(1, 0, 0), theta);
+}
+
+void Quaternion::Rotate_Y(float theta)
+{
+    *this = (*this) * Quaternion(Vector3(0, 1, 0), theta);
+}
+
+void Quaternion::Rotate_Z(float theta)
+{
+    *this = (*this) * Quaternion(Vector3(0, 0, 1), theta);
 }
 
 void Quaternion::Normalize()
@@ -71,29 +83,29 @@ void Quaternion::Normalize()
     }
 }
 
-Matrix3 Quaternion::Build_Matrix3()
+void Quaternion::Randomize()
 {
-    return Matrix3(1.f - 2.f * (j * j + k * k),
-        2.f * (i * j - k * t),
-        2.f * (k * i + j * t),
+    X = ((float)(rand() & 0xFFFF)) / 65536.0f;
+    Y = ((float)(rand() & 0xFFFF)) / 65536.0f;
+    Z = ((float)(rand() & 0xFFFF)) / 65536.0f;
+    W = ((float)(rand() & 0xFFFF)) / 65536.0f;
 
-        2.f * (i * j + k * t),
-        1.f - 2.f * (k * k + i * i),
-        2.f * (j * k - i * t),
-
-        2.f * (k * i - j * t),
-        2.f * (j * k + i * t),
-        1.f - 2.f * (j * j + i * i));
+    Normalize();
 }
 
-Matrix3D Quaternion::Build_Matrix3D()
+Quaternion &Quaternion::Make_Closest(const Quaternion &qto)
 {
-    return Matrix3D(Build_Matrix3(), Vector3(0, 0, 0));
-}
+    float cos_t = qto.X * X + qto.Y * Y + qto.Z * Z + qto.W * W;
 
-Matrix4 Quaternion::Build_Matrix4()
-{
-    return Matrix4(Build_Matrix3D());
+    // if we are on opposite hemisphere from qto, negate ourselves
+    if (cos_t < 0.0) {
+        X = -X;
+        Y = -Y;
+        Z = -Z;
+        W = -W;
+    }
+
+    return *this;
 }
 
 Quaternion Axis_To_Quat(const Vector3 &a, float phi)
@@ -112,7 +124,7 @@ Quaternion Axis_To_Quat(const Vector3 &a, float phi)
     return q;
 }
 
-Quaternion Quaternion::Trackball(float x0, float y0, float x1, float y1, float sphsize)
+Quaternion Trackball(float x0, float y0, float x1, float y1, float sphsize)
 {
     Vector3 a;
     Vector3 p1;
@@ -159,17 +171,15 @@ Quaternion Quaternion::Trackball(float x0, float y0, float x1, float y1, float s
 Quaternion Build_Quaternion(const Matrix3D &mat)
 {
     static int _nxt[3] = {1, 2, 0};
-
     float tr, s;
     int i, j, k;
     Quaternion q;
-
     tr = mat[0][0] + mat[1][1] + mat[2][2];
 
     if (tr > 0.0f) {
-        s = GameMath::Sqrt(tr + 1.0f);
-        q[3] = s * 0.5f;
-        s = 0.5f / s;
+        s = sqrt(tr + 1.0);
+        q[3] = s * 0.5;
+        s = 0.5 / s;
         q[0] = (mat[2][1] - mat[1][2]) * s;
         q[1] = (mat[0][2] - mat[2][0]) * s;
         q[2] = (mat[1][0] - mat[0][1]) * s;
@@ -186,11 +196,99 @@ Quaternion Build_Quaternion(const Matrix3D &mat)
 
         j = _nxt[i];
         k = _nxt[j];
-        s = GameMath::Sqrt((mat[i][i] - (mat[j][j] + mat[k][k])) + 1.0f);
-        q[i] = s * 0.5f;
+        s = sqrt((mat[i][i] - (mat[j][j] + mat[k][k])) + 1.0);
+        q[i] = s * 0.5;
 
-        if (s != 0.0f) {
-            s = 0.5f / s;
+        if (s != 0.0) {
+            s = 0.5 / s;
+        }
+
+        q[3] = (mat[k][j] - mat[j][k]) * s;
+        q[j] = (mat[j][i] + mat[i][j]) * s;
+        q[k] = (mat[k][i] + mat[i][k]) * s;
+    }
+
+    return q;
+}
+
+Quaternion Build_Quaternion(const Matrix3 &mat)
+{
+    static int _nxt[3] = {1, 2, 0};
+    float tr, s;
+    int i, j, k;
+    Quaternion q;
+    tr = mat[0][0] + mat[1][1] + mat[2][2];
+
+    if (tr > 0.0) {
+        s = sqrt(tr + 1.0);
+        q[3] = s * 0.5;
+        s = 0.5 / s;
+
+        q[0] = (mat[2][1] - mat[1][2]) * s;
+        q[1] = (mat[0][2] - mat[2][0]) * s;
+        q[2] = (mat[1][0] - mat[0][1]) * s;
+
+    } else {
+        i = 0;
+
+        if (mat[1][1] > mat[0][0]) {
+            i = 1;
+        }
+
+        if (mat[2][2] > mat[i][i]) {
+            i = 2;
+        }
+
+        j = _nxt[i];
+        k = _nxt[j];
+        s = sqrt((mat[i][i] - (mat[j][j] + mat[k][k])) + 1.0);
+        q[i] = s * 0.5;
+
+        if (s != 0.0) {
+            s = 0.5 / s;
+        }
+
+        q[3] = (mat[k][j] - mat[j][k]) * s;
+        q[j] = (mat[j][i] + mat[i][j]) * s;
+        q[k] = (mat[k][i] + mat[i][k]) * s;
+    }
+
+    return q;
+}
+
+Quaternion Build_Quaternion(const Matrix4 &mat)
+{
+    static int _nxt[3] = {1, 2, 0};
+    float tr, s;
+    int i, j, k;
+    Quaternion q;
+    tr = mat[0][0] + mat[1][1] + mat[2][2];
+
+    if (tr > 0.0) {
+        s = sqrt(tr + 1.0);
+        q[3] = s * 0.5;
+        s = 0.5 / s;
+        q[0] = (mat[2][1] - mat[1][2]) * s;
+        q[1] = (mat[0][2] - mat[2][0]) * s;
+        q[2] = (mat[1][0] - mat[0][1]) * s;
+    } else {
+        i = 0;
+        
+        if (mat[1][1] > mat[0][0]) {
+            i = 1;
+        }
+
+        if (mat[2][2] > mat[i][i]) {
+            i = 2;
+        }
+
+        j = _nxt[i];
+        k = _nxt[j];
+        s = sqrt((mat[i][i] - (mat[j][j] + mat[k][k])) + 1.0);
+        q[i] = s * 0.5;
+
+        if (s != 0.0) {
+            s = 0.5 / s;
         }
 
         q[3] = (mat[k][j] - mat[j][k]) * s;
@@ -205,6 +303,7 @@ Quaternion Build_Quaternion(const Matrix3D &mat)
 void Slerp_Setup(const Quaternion &p, const Quaternion &q, SlerpInfoStruct *slerpinfo)
 {
     float cos_t;
+    captainslog_assert(slerpinfo != NULL);
     cos_t = p.X * q.X + p.Y * q.Y + p.Z * q.Z + p.W * q.W;
 
     if (cos_t < 0.0f) {
@@ -316,6 +415,50 @@ void Fast_Slerp(Quaternion &res, const Quaternion &p, const Quaternion &q, float
     res.W = beta * p.W + alpha * q.W;
 }
 
+void Slerp(Quaternion &res, const Quaternion &p, const Quaternion &q, float alpha)
+{
+    float beta; // complementary interploation parameter
+    float theta; // angle between p and q
+    // float sin_t
+    float cos_t; // sine, cosine of theta
+    float oo_sin_t;
+    int qflip; // use flip of q?
+
+    // cos theta = dot product of p and q
+    cos_t = p.X * q.X + p.Y * q.Y + p.Z * q.Z + p.W * q.W;
+
+    // if q is on opposite hemisphere from A, use -B instead
+    if (cos_t < 0.0f) {
+        cos_t = -cos_t;
+        qflip = true;
+    } else {
+        qflip = false;
+    }
+
+    if (1.0f - cos_t < GAMEMATH_EPSILON * GAMEMATH_EPSILON) {
+        // if q is very close to p, just linearly interpolate
+        // between the two.
+        beta = 1.0f - alpha;
+
+    } else {
+        // normal slerp!
+        theta = GameMath::Acos(cos_t);
+        float sin_t = GameMath::Sin(theta);
+        oo_sin_t = 1.0f / sin_t;
+        beta = GameMath::Sin(theta - alpha * theta) * oo_sin_t;
+        alpha = GameMath::Sin(alpha * theta) * oo_sin_t;
+    }
+
+    if (qflip) {
+        alpha = -alpha;
+    }
+
+    res.X = beta * p.X + alpha * q.X;
+    res.Y = beta * p.Y + alpha * q.Y;
+    res.Z = beta * p.Z + alpha * q.Z;
+    res.W = beta * p.W + alpha * q.W;
+}
+
 Matrix3 Build_Matrix3(const Quaternion &q)
 {
     Matrix3 m;
@@ -332,5 +475,54 @@ Matrix3 Build_Matrix3(const Quaternion &q)
     m[2][1] = (float)(2.0 * (q[1] * q[2] + q[0] * q[3]));
     m[2][2] = (float)(1.0 - 2.0 * (q[1] * q[1] + q[0] * q[0]));
 
+    return m;
+}
+
+Matrix3D Build_Matrix3D(const Quaternion &q)
+{
+    Matrix3D m;
+
+    // initialize the rotation sub-matrix
+    m[0][0] = (float)(1.0 - 2.0 * (q[1] * q[1] + q[2] * q[2]));
+    m[0][1] = (float)(2.0 * (q[0] * q[1] - q[2] * q[3]));
+    m[0][2] = (float)(2.0 * (q[2] * q[0] + q[1] * q[3]));
+
+    m[1][0] = (float)(2.0 * (q[0] * q[1] + q[2] * q[3]));
+    m[1][1] = (float)(1.0 - 2.0f * (q[2] * q[2] + q[0] * q[0]));
+    m[1][2] = (float)(2.0 * (q[1] * q[2] - q[0] * q[3]));
+
+    m[2][0] = (float)(2.0 * (q[2] * q[0] - q[1] * q[3]));
+    m[2][1] = (float)(2.0 * (q[1] * q[2] + q[0] * q[3]));
+    m[2][2] = (float)(1.0 - 2.0 * (q[1] * q[1] + q[0] * q[0]));
+
+    // no translation
+    m[0][3] = m[1][3] = m[2][3] = 0.0f;
+
+    return m;
+}
+
+Matrix4 Build_Matrix4(const Quaternion &q)
+{
+    Matrix4 m;
+
+    // initialize the rotation sub-matrix
+    m[0][0] = (float)(1.0 - 2.0 * (q[1] * q[1] + q[2] * q[2]));
+    m[0][1] = (float)(2.0 * (q[0] * q[1] - q[2] * q[3]));
+    m[0][2] = (float)(2.0 * (q[2] * q[0] + q[1] * q[3]));
+
+    m[1][0] = (float)(2.0 * (q[0] * q[1] + q[2] * q[3]));
+    m[1][1] = (float)(1.0 - 2.0f * (q[2] * q[2] + q[0] * q[0]));
+    m[1][2] = (float)(2.0 * (q[1] * q[2] - q[0] * q[3]));
+
+    m[2][0] = (float)(2.0 * (q[2] * q[0] - q[1] * q[3]));
+    m[2][1] = (float)(2.0 * (q[1] * q[2] + q[0] * q[3]));
+    m[2][2] = (float)(1.0 - 2.0 * (q[1] * q[1] + q[0] * q[0]));
+
+    // no translation
+    m[0][3] = m[1][3] = m[2][3] = 0.0f;
+
+    // last row
+    m[3][0] = m[3][1] = m[3][2] = 0.0f;
+    m[3][3] = 1.0f;
     return m;
 }
--- src/w3d/math/quaternion.h
+++ src/w3d/math/quaternion.h
@@ -52,20 +52,15 @@ public:
         Z = c;
         W = d;
     }
-    Quaternion(float angle, const Vector3 &axis);
+    __forceinline explicit Quaternion(const Vector3 &axis, float angle);
+
+    __forceinline Quaternion &operator=(const Quaternion &source);
 
     void Normalize();
 
     float &operator[](int i) { return (&X)[i]; }
     const float &operator[](int i) const { return (&X)[i]; }
 
-    Quaternion conjugate() { return Quaternion(t, -i, -j, -k); }
-
-    Matrix3 Build_Matrix3();
-    Matrix3D Build_Matrix3D();
-    Matrix4 Build_Matrix4();
-
-    static Quaternion Trackball(float x0, float y0, float x1, float y1, float sphsize);
     void Scale(float s)
     {
         X = (float)(s * X);
@@ -74,56 +69,40 @@ public:
         W = (float)(s * W);
     }
 
-    friend Quaternion operator*(const Vector3 &a, const Quaternion &b);
-    friend Quaternion operator*(const Quaternion &a, const Quaternion &b);
-
     bool Is_Valid() const;
     __forceinline void Make_Identity() { Set(); }
-    __forceinline void Set(float a = 0.0f, float b = 0.0f, float c = 0.0f, float d = 1.0f)
+    __forceinline void Set(float a = 0.0, float b = 0.0, float c = 0.0, float d = 1.0)
     {
         X = a;
         Y = b;
         Z = c;
         W = d;
     }
+
     Vector3 Rotate_Vector(const Vector3 &v) const;
+    void Rotate_Vector(const Vector3 &v, Vector3 *set_result) const;
+    __forceinline Quaternion operator-() const { return (Quaternion(-X, -Y, -Z, -W)); }
+    __forceinline Quaternion operator+() const { return *this; }
+    Quaternion &Make_Closest(const Quaternion &qto);
+    __forceinline float Length2() const { return (X * X + Y * Y + Z * Z + W * W); }
+    __forceinline float Length() const { return GameMath::Sqrt(Length2()); }
+    void Rotate_X(float theta);
+    void Rotate_Y(float theta);
+    void Rotate_Z(float theta);
+    void Randomize();
 
 public:
-    static const Quaternion IDENTITY;
-
-    union
-    {
-        struct
-        {
-            float X;
-            float Y;
-            float Z;
-            float W;
-        };
-
-        struct
-        {
-            float i;
-            float j;
-            float k;
-            float t;
-        };
-    };
+    float X;
+    float Y;
+    float Z;
+    float W;
 };
-
-__forceinline Quaternion operator*(const Vector3 &a, const Quaternion &b)
-{
-    return Quaternion(a.X, a.Y, a.Z, 0) * b;
-}
 
 __forceinline Quaternion operator*(const Quaternion &a, const Quaternion &b)
 {
-    // Rv = Ar*Bv + Br*Av + Av cross Bv
-    // Rr = Ar*Br + Av dot Bv
-
-    return Quaternion(a.W * b.X + b.W * a.X + (a.Y * b.Z - a.Z * b.Y),
-        a.W * b.Y + b.W * a.Y + (a.Z * b.X - a.X * b.Z),
-        a.W * b.Z + b.W * a.Z + (a.X * b.Y - a.Y * b.X),
+    return Quaternion(a.W * b.X + b.W * a.X + (a.Y * b.Z - b.Y * a.Z),
+        a.W * b.Y + b.W * a.Y - (a.X * b.Z - b.X * a.Z),
+        a.W * b.Z + b.W * a.Z + (a.X * b.Y - b.X * a.Y),
         a.W * b.W - (a.X * b.X + a.Y * b.Y + a.Z * b.Z));
 }
 
@@ -143,6 +122,30 @@ __forceinline Vector3 Quaternion::Rotate_Vector(const Vector3 &v) const
     return Vector3(w * (-X) + W * x + (y * (-Z) - (-Y) * z),
         w * (-Y) + W * y - (x * (-Z) - (-X) * z),
         w * (-Z) + W * z + (x * (-Y) - (-X) * y));
+}
+
+__forceinline void Quaternion::Rotate_Vector(const Vector3 &v, Vector3 *result) const
+{
+    captainslog_assert(result != NULL);
+
+    float x = W * v.X + (Y * v.Z - v.Y * Z);
+    float y = W * v.Y - (X * v.Z - v.X * Z);
+    float z = W * v.Z + (X * v.Y - v.X * Y);
+    float w = -(X * v.X + Y * v.Y + Z * v.Z);
+
+    result->X = w * (-X) + W * x + (y * (-Z) - (-Y) * z);
+    result->Y = w * (-Y) + W * y - (x * (-Z) - (-X) * z);
+    result->Z = w * (-Z) + W * z + (x * (-Y) - (-X) * y);
+}
+
+__forceinline Quaternion &Quaternion::operator=(const Quaternion &source)
+{
+    X = source[0];
+    Y = source[1];
+    Z = source[2];
+    W = source[3];
+
+    return *this;
 }
 
 __forceinline Quaternion Inverse(const Quaternion &a)
@@ -165,9 +168,48 @@ __forceinline Quaternion operator*(const Quaternion &a, float scl)
     return scl * a;
 }
 
-Quaternion Build_Quaternion(const Matrix3D &mat);
+__forceinline Quaternion Conjugate(const Quaternion &a)
+{
+    return Quaternion(-a[0], -a[1], -a[2], a[3]);
+}
+
+__forceinline Quaternion operator+(const Quaternion &a, const Quaternion &b)
+{
+    return Quaternion(a[0] + b[0], a[1] + b[1], a[2] + b[2], a[3] + b[3]);
+}
+
+__forceinline Quaternion operator-(const Quaternion &a, const Quaternion &b)
+{
+    return Quaternion(a[0] - b[0], a[1] - b[1], a[2] - b[2], a[3] - b[3]);
+}
+
+__forceinline Quaternion Normalize(const Quaternion &a)
+{
+    float mag = a.Length();
+    if (0.0f == mag) {
+        return a;
+    } else {
+        float oomag = 1.0f / mag;
+        return Quaternion(a[0] * oomag, a[1] * oomag, a[2] * oomag, a[3] * oomag);
+    }
+}
+
+__forceinline bool Equal_Within_Epsilon(const Quaternion &a, const Quaternion &b, float epsilon)
+{
+    return ((GameMath::Fabs(a.X - b.X) < epsilon) && (GameMath::Fabs(a.Y - b.Y) < epsilon)
+        && (GameMath::Fabs(a.Z - b.Z) < epsilon) && (GameMath::Fabs(a.W - b.W) < epsilon));
+}
+
+Quaternion Axis_To_Quat(const Vector3 &a, float angle);
+Quaternion Trackball(float x0, float y0, float x1, float y1, float sphsize);
+void Slerp(Quaternion &result, const Quaternion &a, const Quaternion &b, float t);
+void Fast_Slerp(Quaternion &result, const Quaternion &a, const Quaternion &b, float t);
+Quaternion Build_Quaternion(const Matrix3 &matrix);
+Quaternion Build_Quaternion(const Matrix3D &matrix);
+Quaternion Build_Quaternion(const Matrix4 &matrix);
+Matrix3 Build_Matrix3(const Quaternion &quat);
+Matrix3D Build_Matrix3D(const Quaternion &quat);
+Matrix4 Build_Matrix4(const Quaternion &quat);
 void Slerp_Setup(const Quaternion &p, const Quaternion &q, SlerpInfoStruct *slerpinfo);
 void Cached_Slerp(const Quaternion &p, const Quaternion &q, float alpha, SlerpInfoStruct *slerpinfo, Quaternion *set_q);
 Quaternion Cached_Slerp(const Quaternion &p, const Quaternion &q, float alpha, SlerpInfoStruct *slerpinfo);
-void Fast_Slerp(Quaternion &result, const Quaternion &a, const Quaternion &b, float t);
-Matrix3 Build_Matrix3(const Quaternion &quat);

aa5f8fd9636a4ca6d1cb89abe7b16a1c7d691b00
Smileynator <->
2020-07-13 21:16:41 +0200

    [Clang] Reformat of all documents associated previous commit with Clang.

--- src/game/common/system/asciistring.cpp
+++ src/game/common/system/asciistring.cpp
@@ -24,9 +24,7 @@ Utf8String const Utf8String::s_emptyString(nullptr);
 /**
  * Default constructor added for convenience
  */
-Utf8String::Utf8String() : m_data(nullptr)
-{
-}
+Utf8String::Utf8String() : m_data(nullptr) {}
 
 /**
  * Inits this string with an existing string (copy) and increments reference count
@@ -43,11 +41,11 @@ Utf8String::Utf8String(Utf8String const &string) : m_data(string.m_data)
  */
 Utf8String::Utf8String(const char *s) : m_data(nullptr)
 {
-    if ( s != nullptr ) {
-		//Get length of the string that was passed
+    if (s != nullptr) {
+        // Get length of the string that was passed
         int len = (int)strlen(s);
-        if ( len > 0 ) {
-			//Allocate a buffer
+        if (len > 0) {
+            // Allocate a buffer
             Ensure_Unique_Buffer_Of_Size(len + 1, false, s);
         }
     }
@@ -59,7 +57,7 @@ Utf8String::Utf8String(const char *s) : m_data(nullptr)
 char *Utf8String::Peek() const
 {
     captainslog_dbgassert(m_data != nullptr, "null string ptr");
-    
+
     return m_data->Peek();
 }
 
@@ -68,9 +66,9 @@ char *Utf8String::Peek() const
  */
 void Utf8String::Release_Buffer()
 {
-    if ( m_data != nullptr ) {
+    if (m_data != nullptr) {
         m_data->Dec_Ref_Count();
-        if ( m_data->ref_count == 0 ) {
+        if (m_data->ref_count == 0) {
             Free_Bytes();
         }
         m_data = nullptr;
@@ -80,54 +78,55 @@ void Utf8String::Release_Buffer()
 /**
  * Frees the allocated memory for this string. Thanks for all the fish!
  */
-void Utf8String::Free_Bytes() 
+void Utf8String::Free_Bytes()
 {
-    g_dynamicMemoryAllocator->Free_Bytes(m_data); 
+    g_dynamicMemoryAllocator->Free_Bytes(m_data);
 }
 
 /**
  * Allocates a memory buffer for a string's content.
  */
-void Utf8String::Ensure_Unique_Buffer_Of_Size(int chars_needed, bool keep_data, const char *str_to_cpy, const char *str_to_cat)
+void Utf8String::Ensure_Unique_Buffer_Of_Size(
+    int chars_needed, bool keep_data, const char *str_to_cpy, const char *str_to_cat)
 {
-    if ( m_data != nullptr && m_data->ref_count == 1 && m_data->num_chars_allocated >= chars_needed ) {
-        if ( str_to_cpy != nullptr ) {
+    if (m_data != nullptr && m_data->ref_count == 1 && m_data->num_chars_allocated >= chars_needed) {
+        if (str_to_cpy != nullptr) {
             strcpy(Peek(), str_to_cpy);
         }
 
-        if ( str_to_cat != nullptr ) {
+        if (str_to_cat != nullptr) {
             strcpy(Peek() + strlen(Peek()), str_to_cat);
         }
 
     } else {
-
-        //this block would have been a macro like DEBUG_CRASH(numCharsNeeded + 8 > MAX_LEN, THROW_02); (see cl_debug.h)
-        //if ( numCharsNeeded + 8 > MAX_LEN ) {
-            // *&preserveData = 0xDEAD0002;
-            //throw(&preserveData, &_TI1_AW4ErrorCode__);
+        // this block would have been a macro like DEBUG_CRASH(numCharsNeeded + 8 > MAX_LEN, THROW_02); (see cl_debug.h)
+        // if ( numCharsNeeded + 8 > MAX_LEN ) {
+        // *&preserveData = 0xDEAD0002;
+        // throw(&preserveData, &_TI1_AW4ErrorCode__);
         //}
 
         int size = g_dynamicMemoryAllocator->Get_Actual_Allocation_Size(chars_needed + sizeof(AsciiStringData));
-        AsciiStringData *new_data = reinterpret_cast<AsciiStringData *>(g_dynamicMemoryAllocator->Allocate_Bytes_No_Zero(size));
-        
+        AsciiStringData *new_data =
+            reinterpret_cast<AsciiStringData *>(g_dynamicMemoryAllocator->Allocate_Bytes_No_Zero(size));
+
         new_data->ref_count = 1;
         new_data->num_chars_allocated = size - sizeof(AsciiStringData);
-        //new_data->num_chars_allocated = numCharsNeeded;
-    #ifdef GAME_DEBUG_STRUCTS
+        // new_data->num_chars_allocated = numCharsNeeded;
+#ifdef GAME_DEBUG_STRUCTS
         new_data->debug_ptr = new_data->Peek();
-    #endif
+#endif
 
-        if ( m_data != nullptr && keep_data ) {
+        if (m_data != nullptr && keep_data) {
             strcpy(new_data->Peek(), Peek());
         } else {
             *new_data->Peek() = '\0';
         }
 
-        if ( str_to_cpy != nullptr ) {
+        if (str_to_cpy != nullptr) {
             strcpy(new_data->Peek(), str_to_cpy);
         }
 
-        if ( str_to_cat != nullptr ) {
+        if (str_to_cat != nullptr) {
             strcat(new_data->Peek(), str_to_cat);
         }
 
@@ -137,17 +136,17 @@ void Utf8String::Ensure_Unique_Buffer_Of_Size(int chars_needed, bool keep_data, 
 }
 
 /**
-* Gets the length of the string
-*/
+ * Gets the length of the string
+ */
 int Utf8String::Get_Length() const
 {
-    if ( m_data != nullptr ) {
+    if (m_data != nullptr) {
         int len = strlen(Str());
         captainslog_dbgassert(len > 0, "length of string is less than or equal to 0.");
-        
+
         return len;
     }
-    
+
     return 0;
 }
 
@@ -155,7 +154,7 @@ char Utf8String::Get_Char(int index) const
 {
     captainslog_dbgassert(index >= 0, "bad index in getCharAt.");
     captainslog_dbgassert(Get_Length() > 0, "strlen returned less than or equal to 0 in getCharAt.");
-    
+
     return Peek()[index];
 }
 
@@ -166,10 +165,10 @@ const char *Utf8String::Str() const
 {
     static char const TheNullChr[4] = "";
 
-    if ( m_data != nullptr ) {
+    if (m_data != nullptr) {
         return Peek();
     }
-    
+
     return TheNullChr;
 }
 
@@ -187,10 +186,10 @@ char *Utf8String::Get_Buffer_For_Read(int len)
  */
 void Utf8String::Set(const char *c)
 {
-    if ( m_data == nullptr || c != m_data->Peek() ) {
+    if (m_data == nullptr || c != m_data->Peek()) {
         size_t len;
-        
-        if ( c && (len = strlen(c) + 1, len != 1) ) {
+
+        if (c && (len = strlen(c) + 1, len != 1)) {
             Ensure_Unique_Buffer_Of_Size(len, false, c, nullptr);
         } else {
             Release_Buffer();
@@ -203,11 +202,11 @@ void Utf8String::Set(const char *c)
  */
 void Utf8String::Set(Utf8String const &string)
 {
-    if ( &string != this ) {
+    if (&string != this) {
         Release_Buffer();
         m_data = string.m_data;
-        
-        if ( m_data != nullptr ) {
+
+        if (m_data != nullptr) {
             m_data->Inc_Ref_Count();
         }
     }
@@ -219,7 +218,7 @@ void Utf8String::Set(Utf8String const &string)
 void Utf8String::Translate(Utf16String const &string)
 {
     Release_Buffer();
-    
+
 #if defined BUILD_WITH_ICU // Use ICU convertors
     int32_t length;
     UErrorCode error = U_ZERO_ERROR;
@@ -241,8 +240,8 @@ void Utf8String::Translate(Utf16String const &string)
 #else // Naive copy, this is what the original does.
     int str_len = string.Get_Length();
 
-    for ( int i = 0; i < str_len; ++i ) {
-        //This is a debug assert from the look of it.
+    for (int i = 0; i < str_len; ++i) {
+        // This is a debug assert from the look of it.
         /*if ( v4 < 8 || (!stringSrc.m_data ? (v6 = 0) : (v5 = stringSrc.Peek(), v6 = wcslen(v5)), v3 >= v6) )
         {
             if ( `Utf16String::Get_Char'::`14'::allowCrash )
@@ -254,12 +253,12 @@ void Utf8String::Translate(Utf16String const &string)
         }*/
         wchar_t c;
 
-        if ( string.m_data != nullptr ) {
+        if (string.m_data != nullptr) {
             c = string.Get_Char(i);
         } else {
             c = L'\0';
         }
-        
+
         // null out the second byte so Concat only concatenates the first byte.
         // prevents issues if unicode string contains none ascii chars.
         // This will have endian issues on big endian.
@@ -287,7 +286,7 @@ void Utf8String::Translate(Utf16String const &string)
 void Utf8String::Concat(char c)
 {
     char str[2];
- 
+
     str[0] = c;
     str[1] = '\0';
     Concat(str);
@@ -300,8 +299,8 @@ void Utf8String::Concat(const char *s)
 {
     int len = strlen(s);
 
-    if ( len > 0 ) {
-        if ( m_data != nullptr ) {
+    if (len > 0) {
+        if (m_data != nullptr) {
             Ensure_Unique_Buffer_Of_Size(strlen(Peek()) + len + 1, true, 0, s);
         } else {
             Set(s);
@@ -315,31 +314,31 @@ void Utf8String::Concat(const char *s)
 void Utf8String::Trim()
 {
     // No string, no Trim.
-    if ( m_data == nullptr ) {
+    if (m_data == nullptr) {
         return;
     }
 
     char *str = Peek();
 
     // Find first none space in string if not the first.
-    for ( char i = *str; i != '\0'; i = *(++str) ) {
-        if ( !isspace(i) ) {
+    for (char i = *str; i != '\0'; i = *(++str)) {
+        if (!isspace(i)) {
             break;
         }
     }
 
     // If we had some spaces and moved Str, Set string to that position.
-    if ( str != Peek() ) {
+    if (str != Peek()) {
         Set(str);
     }
 
     // Oops, Set call broke the string.
-    if ( m_data == nullptr ) {
+    if (m_data == nullptr) {
         return;
     }
 
-    for ( int i = strlen(Peek()) - 1; i >= 0; --i ) {
-        if ( !isspace(Get_Char(i)) ) {
+    for (int i = strlen(Peek()) - 1; i >= 0; --i) {
+        if (!isspace(Get_Char(i))) {
             break;
         }
 
@@ -355,13 +354,13 @@ void Utf8String::To_Lower()
     // Size specifically matches original code for compatability
     char buf[MAX_TO_LOWER_BUF_LEN];
 
-    if ( m_data == nullptr ) {
+    if (m_data == nullptr) {
         return;
     }
 
     strcpy(buf, Peek());
 
-    for ( char *c = buf; *c != '\0'; ++c ) {
+    for (char *c = buf; *c != '\0'; ++c) {
         *c = tolower(*c);
     }
 
@@ -375,14 +374,14 @@ Utf8String Utf8String::Posix_Path() const
 {
     char buf[MAX_FORMAT_BUF_LEN];
 
-    if ( m_data == nullptr ) {
+    if (m_data == nullptr) {
         return s_emptyString;
     }
 
     strcpy(buf, Peek());
 
-    for ( char *c = buf; *c != '\0'; ++c ) {
-        if ( *c == '\\' ) {
+    for (char *c = buf; *c != '\0'; ++c) {
+        if (*c == '\\') {
             *c = '/';
         }
     }
@@ -417,13 +416,13 @@ Utf8String Utf8String::Windows_Path() const
  */
 void Utf8String::Remove_Last_Char()
 {
-    if ( m_data == nullptr ) {
+    if (m_data == nullptr) {
         return;
     }
 
     int len = strlen(Peek());
 
-    if ( len > 0 ) {
+    if (len > 0) {
         Ensure_Unique_Buffer_Of_Size(len + 1, true);
         Peek()[len - 1] = '\0';
     }
@@ -483,17 +482,17 @@ void Utf8String::Format_VA(Utf8String &format, va_list args)
  */
 bool Utf8String::Starts_With(const char *p) const
 {
-    if ( *p == '\0' ) {
+    if (*p == '\0') {
         return true;
     }
     // early out if our string is shorter than the input one
     int thislen = m_data != nullptr ? strlen(Peek()) : 0;
     int thatlen = strlen(p);
-    
-    if ( thislen < thatlen ) {
+
+    if (thislen < thatlen) {
         return false;
     }
-    
+
     return strncmp(Peek(), p, thatlen) == 0;
 }
 
@@ -502,17 +501,17 @@ bool Utf8String::Starts_With(const char *p) const
  */
 bool Utf8String::Ends_With(const char *p) const
 {
-    if ( *p == '\0' ) {
+    if (*p == '\0') {
         return true;
     }
-    //early out if our string is shorter than the input one
+    // early out if our string is shorter than the input one
     int thislen = m_data != nullptr ? strlen(Peek()) : 0;
     int thatlen = strlen(p);
-    
-    if ( thislen < thatlen ) {
+
+    if (thislen < thatlen) {
         return false;
     }
-    //compare strings
+    // compare strings
     return strncmp(Peek() + thislen - thatlen, p, thatlen) == 0;
 }
 
@@ -521,17 +520,17 @@ bool Utf8String::Ends_With(const char *p) const
  */
 bool Utf8String::Starts_With_No_Case(const char *p) const
 {
-    if ( *p == '\0' ) {
+    if (*p == '\0') {
         return true;
     }
     // early out if our string is shorter than the input one
     int thislen = Get_Length();
     int thatlen = strlen(p);
-    
-    if ( thislen < thatlen ) {
+
+    if (thislen < thatlen) {
         return false;
     }
-    
+
     return strncasecmp(Peek(), p, thatlen) == 0;
 }
 
@@ -540,77 +539,77 @@ bool Utf8String::Starts_With_No_Case(const char *p) const
  */
 bool Utf8String::Ends_With_No_Case(const char *p) const
 {
-    if ( *p == '\0' ) {
+    if (*p == '\0') {
         return true;
     }
-    
+
     int thislen = m_data != nullptr ? strlen(Peek()) : 0;
     int thatlen = strlen(p);
-    
-    if ( thislen < thatlen ) {
+
+    if (thislen < thatlen) {
         return false;
     }
-    
+
     return strncasecmp(Peek() + thislen - thatlen, p, thatlen) == 0;
 }
 
 /**
-* Find the next tokens and loads *tok with the string between these token
-* The string this is used on is consumed in the process. (a primitive string split)
-*/
+ * Find the next tokens and loads *tok with the string between these token
+ * The string this is used on is consumed in the process. (a primitive string split)
+ */
 bool Utf8String::Next_Token(Utf8String *tok, const char *delims)
 {
-    if ( m_data == nullptr ) {
+    if (m_data == nullptr) {
         return false;
     }
-    
-    if ( *Peek() == '\0' || this == tok ) {
+
+    if (*Peek() == '\0' || this == tok) {
         return false;
     }
-    
+
     //
     // If no separators provided, default to white space.
     //
-    if ( delims == nullptr ) {
+    if (delims == nullptr) {
         delims = " \n\r\t";
     }
-    
+
     char *start = Peek();
-    
+
     //
     // Find next instance of token or end of string
     //
-    for ( char c = *start; c != '\0'; c = *(++start) ) {
-        if ( strchr(delims, c) == nullptr ) {
+    for (char c = *start; c != '\0'; c = *(++start)) {
+        if (strchr(delims, c) == nullptr) {
             break;
         }
     }
-    
-    if ( *start == '\0' ) {
+
+    if (*start == '\0') {
         Release_Buffer();
         tok->Release_Buffer();
-        
+
         return false;
     }
-    
+
     char *end = start;
-    
+
     //
     // Find next instance of token or end of string.
     //
-    for ( char c = *end; c != '\0'; c = *(++end) ) {
-        if ( strchr(delims, c) != nullptr ) {
+    for (char c = *end; c != '\0'; c = *(++end)) {
+        if (strchr(delims, c) != nullptr) {
             break;
         }
     }
-    
-    if ( end <= start ) {
+
+    if (end <= start) {
         Release_Buffer();
         tok->Release_Buffer();
-        
+
         return false;
     }
-    
+
     //
     // Copy found region into provided Utf8String, then move this string
     // to start of next section.
@@ -619,7 +618,7 @@ bool Utf8String::Next_Token(Utf8String *tok, const char *delims)
     memcpy(tokstr, start, end - start);
     tokstr[end - start] = '\0';
     Set(end);
-    
+
     return true;
 }
 
@@ -631,6 +630,6 @@ void Utf8String::Validate()
 #ifdef GAME_DEBUG
 void Utf8String::Debug_Ignore_Leaks()
 {
-    //TODO, doesnt seem to be implimented anywhere? It is called though...
+    // TODO, doesnt seem to be implimented anywhere? It is called though...
 }
 #endif // GAME_DEBUG

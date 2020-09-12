8e59ec3623c717666e58ab2ad99a50b9de486a71
Toms <tomsons26lv@gmail.com>
2020-09-12 18:53:10 +0300

    Implements FontLibrary.

--- src/game/client/gui/gamefont.cpp
+++ src/game/client/gui/gamefont.cpp
@@ -1,9 +1,10 @@
-/**
+﻿/**
  * @file
  *
  * @author OmniBlade
+ * @author tomsons26
  *
- * @brief Information relating to a font to use in game.
+ * @brief Font information and handling classes.
  *
  * @copyright Thyme is free software: you can redistribute it and/or
  *            modify it under the terms of the GNU General Public License
@@ -13,3 +14,104 @@
  *            LICENSE
  */
 #include "gamefont.h"
+
+FontLibrary::FontLibrary() : m_fontList(nullptr), m_count(0) {}
+
+FontLibrary::~FontLibrary()
+{
+    Delete_All_Fonts();
+}
+
+void FontLibrary::Reset()
+{
+    Delete_All_Fonts();
+}
+
+void FontLibrary::Link_Font(GameFont *font)
+{
+    if (font != nullptr) {
+        font->m_next = m_fontList;
+        m_fontList = font;
+        ++m_count;
+    }
+}
+
+void FontLibrary::Unlink_Font(GameFont *font)
+{
+    if (font == nullptr) {
+        return;
+    }
+
+    GameFont *i = m_fontList;
+    while (i != nullptr) {
+        if (i == font) {
+            break;
+        }
+        i = i->m_next;
+    }
+
+    if (i == nullptr) {
+        captainslog_fatal("Font '%s' not found in library", font->m_nameString.Str());
+        return;
+    }
+
+    i = m_fontList;
+    while (i != nullptr) {
+        if (i->m_next == font) {
+            break;
+        }
+
+        i = i->m_next;
+    }
+
+    if (i == nullptr) {
+        m_fontList = font->m_next;
+    } else {
+        i->m_next = font->m_next;
+    }
+
+    font->m_next = nullptr;
+    --m_count;
+}
+
+void FontLibrary::Delete_All_Fonts()
+{
+    while (m_fontList != nullptr) {
+        GameFont *fnt = m_fontList;
+        Unlink_Font(m_fontList);
+        Release_Font_Data(fnt);
+        Delete_Instance(fnt);
+    }
+}
+
+GameFont *FontLibrary::Get_Font(Utf8String name, int point_size, bool bold)
+{
+    GameFont *font = nullptr;
+
+    for (font = m_fontList; font != nullptr; font = font->m_next) {
+        if (font->m_nameString == name && font->m_pointSize == point_size && font->m_bold == bold) {
+            return font;
+        }
+    }
+
+    font = new GameFont;
+    if (font == nullptr) {
+        captainslog_fatal("Unable to allocate new font list element");
+        return nullptr;
+    }
+
+    font->m_nameString = name;
+    font->m_pointSize = point_size;
+    font->m_bold = bold;
+    font->m_fontData = nullptr;
+
+    captainslog_debug("Loading font '%s' with %d point size", name.Str(), point_size);
+    if (!Load_Font_Data(font)) {
+        captainslog_fatal("Unable to load font data pointer '%s'", name.Str());
+        Delete_Instance(font);
+        return nullptr;
+    }
+
+    Link_Font(font);
+    return font;
+}

cc6adb9d3cf33e07ffa8bb1e09ea31d4b608d9e6
Jonathan Wilson <jfwfreo@tpgi.com.au>
2020-11-15 13:47:46 +1000

    Texture Mappers

---
+++ src/w3d/lib/buff.h
@@ -0,0 +1,40 @@
+/**
+ * @file
+ *
+ * @author Jonathan Wilson
+ *
+ * @brief Buffer Class
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+#include "always.h"
+class Buffer
+{
+public:
+    Buffer(char *ptr, long size = 0);
+    Buffer(void *ptr = 0, long size = 0);
+    Buffer(void const *ptr, long size = 0);
+    Buffer(long size);
+    Buffer(Buffer const &buffer);
+    ~Buffer();
+
+    Buffer &operator=(Buffer const &buffer);
+    operator void *() const { return m_bufferPtr; }
+    operator char *() const { return (char *)m_bufferPtr; }
+
+    void Reset();
+    void *Get_Buffer() const { return m_bufferPtr; }
+    long Get_Size() const { return m_size; }
+    bool Is_Valid() const { return m_bufferPtr != 0; }
+
+protected:
+    void *m_bufferPtr;
+    long m_size;
+    bool m_isAllocated;
+};

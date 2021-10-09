2b66e959b048004a1c018cbf1bde6565922bcbaa
xezon <4720891+xezon@users.noreply.github.com>
2021-10-09 20:47:09 +0200

    Set CMAKE_VS_INCLUDE_INSTALL_TO_DEFAULT_BUILD=1 option in build scripts

--- tools/build/win_x86/create_vs2017_solution.bat
+++ tools/build/win_x86/create_vs2017_solution.bat
@@ -4,12 +4,16 @@ set ThisDir=%~dp0.
 call "%ThisDir%\scripts\setup_folders.bat"
 
 :: Extract tools if necessary
-call "%BuildDir%\scripts\extract_tools.bat"
+call "%ThisDir%\scripts\extract_tools.bat"
 
 if not exist "%SolutionVS2017Dir%" mkdir "%SolutionVS2017Dir%"
 cd "%SolutionVS2017Dir%"
 :: Generate solution files
-"%CMakeDir%\bin\cmake.exe" -G"Visual Studio 15 2017" -A Win32 -S "%ProjectDir%"
+"%CMakeDir%\bin\cmake.exe" ^
+    -G"Visual Studio 15 2017" ^
+    -A Win32 ^
+    -S "%ProjectDir%" ^
+    -DCMAKE_VS_INCLUDE_INSTALL_TO_DEFAULT_BUILD=1
 :: Open cmake gui for user customizations
 if not ERRORLEVEL 1 (
     "%CMakeDir%\bin\cmake-gui.exe" "%SolutionVS2017Dir%"
--- tools/build/win_x86/create_vs2019_solution.bat
+++ tools/build/win_x86/create_vs2019_solution.bat
@@ -4,12 +4,16 @@ set ThisDir=%~dp0.
 call "%ThisDir%\scripts\setup_folders.bat"
 
 :: Extract tools if necessary
-call "%BuildDir%\scripts\extract_tools.bat"
+call "%ThisDir%\scripts\extract_tools.bat"
 
 if not exist "%SolutionVS2019Dir%" mkdir "%SolutionVS2019Dir%"
 cd "%SolutionVS2019Dir%"
 :: Generate solution files
-"%CMakeDir%\bin\cmake.exe" -G"Visual Studio 16 2019" -A Win32 -S "%ProjectDir%"
+"%CMakeDir%\bin\cmake.exe" ^
+    -G"Visual Studio 16 2019" ^
+    -A Win32 ^
+    -S "%ProjectDir%" ^
+    -DCMAKE_VS_INCLUDE_INSTALL_TO_DEFAULT_BUILD=1
 :: Open cmake gui for user customizations
 if not ERRORLEVEL 1 (
     "%CMakeDir%\bin\cmake-gui.exe" "%SolutionVS2019Dir%"

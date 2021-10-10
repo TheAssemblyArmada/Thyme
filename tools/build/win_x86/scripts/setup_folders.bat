1eda2760d151cfd1e46153551ee70c64ade7125b
xezon <4720891+xezon@users.noreply.github.com>
2021-10-10 02:12:16 +0200

    Add cmake build scripts for Windows and Visual Studio

---
+++ tools/build/win_x86/scripts/setup_folders.bat
@@ -0,0 +1,5 @@
+set ProjectDir=%~dp0..\..\..\..
+set SolutionVS2019Dir=%ProjectDir%\.solution\vs2019
+set SolutionVS2017Dir=%ProjectDir%\.solution\vs2017
+set ToolsDir=%ProjectDir%\tools
+set CMakeDir=%ToolsDir%\.cmake

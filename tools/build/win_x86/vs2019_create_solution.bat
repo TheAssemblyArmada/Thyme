8df675543136c3368476b67cdead1b685422e17b
xezon <4720891+xezon@users.noreply.github.com>
2021-10-13 20:41:09 +0200

    Move the generic win scripts next to win_x86

--- tools/build/win_x86/vs2019_create_solution.bat
+++ tools/build/win_x86/vs2019_create_solution.bat
@@ -1,7 +1,7 @@
-setlocal
+@setlocal
 
-@set ThisDir=%~dp0.
-@call "%ThisDir%\scripts\setup_folders.bat"
-@call "%ThisDir%\scripts\vs_create_solution.bat" "%SolutionVS2019Dir%" "Visual Studio 16 2019"
+@set BuildDir=%~dp0..
+@call "%BuildDir%\win_scripts\setup_folders.bat"
+@call "%BuildDir%\win_scripts\vs_create_solution.bat" "%SolutionVS2019Dir%" "Visual Studio 16 2019"
 
-endlocal
+@endlocal

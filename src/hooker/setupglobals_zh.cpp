f88b1db9f85b514d3e2f6bbd04e15358e6f18faa
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2021-10-19 16:58:36 +0100

    Apply review comments

--- src/hooker/setupglobals_zh.cpp
+++ src/hooker/setupglobals_zh.cpp
@@ -471,7 +471,7 @@ MouseThreadClass &W3DMouse::s_mouseThread = Make_Global<MouseThreadClass>(0x00A3
 bool &W3DMouse::s_mouseThreadIsDrawing = Make_Global<bool>(0x00A3B3C8);
 ARRAY_DEF(0x00A3B18C, HAnimClass *, W3DMouse::s_W3DMouseAssets1, CURSOR_COUNT);
 ARRAY_DEF(0x00A3B22C, RenderObjClass *, W3DMouse::s_W3DMouseAssets2, CURSOR_COUNT);
-ARRAY2D_DEF(0x00A3B3D8, TextureBaseClass *, W3DMouse::s_D3DMouseAssets, CURSOR_COUNT, 21); // TODO unsure on type
+ARRAY2D_DEF(0x00A3B3D8, TextureBaseClass *, W3DMouse::s_D3DMouseAssets, CURSOR_COUNT, W3DMouse::MAX_FRAMES); // TODO unsure on type
 ARRAY_DEF(0x00A3B2CC, uint32_t, W3DMouse::s_PolyMouseAssets, CURSOR_COUNT); // unsure on type
 CriticalSectionClass &g_mouseCriticalSection = Make_Global<CriticalSectionClass>(0x00A3B3D0);
 
--- src/platform/w3dengine/client/w3dmouse.h
+++ src/platform/w3dengine/client/w3dmouse.h
@@ -43,22 +43,24 @@ public:
 
     static void Set_Is_Drawing(bool isDrawing) { s_mouseThreadIsDrawing = isDrawing; }
 
+    static constexpr size_t MAX_FRAMES = 21;
+
 private:
     void Free_W3D_Assets();
     void Init_W3D_Assets();
     void Free_D3D_Assets();
     void Init_D3D_Assets();
     void Release_D3D_Cursor_Texture(MouseCursor cursor);
-    bool Load_D3D_Cursor_Texture(MouseCursor cursor);
+    bool Load_D3D_Cursor_Textures(MouseCursor cursor);
     void Init_Polygon_Assets();
     void Set_Cursor_Direction(MouseCursor cursor);
 
     MouseCursor m_currentD3DCursor;
-    SurfaceClass *m_D3DCursorSurfaces[21];
+    SurfaceClass *m_D3DCursorSurfaces[MAX_FRAMES];
     int32_t m_hotSpotX;
     int32_t m_hotSpotY;
     int32_t m_D3DCursorSurfaceCount;
-    int32_t unk4; // float
+    float unk4;
     int32_t unk5;
     int32_t m_D3DCursorLastDrawn;
     float unk7; // refersh rate related fps * 0.001
@@ -72,14 +74,14 @@ private:
     static bool &s_mouseThreadIsDrawing;
     static ARRAY_DEC(HAnimClass *, s_W3DMouseAssets1, CURSOR_COUNT);
     static ARRAY_DEC(RenderObjClass *, s_W3DMouseAssets2, CURSOR_COUNT);
-    static ARRAY2D_DEC(TextureBaseClass *, s_D3DMouseAssets, CURSOR_COUNT, 21); // TODO unsure on type
+    static ARRAY2D_DEC(TextureBaseClass *, s_D3DMouseAssets, CURSOR_COUNT, MAX_FRAMES); // TODO unsure on type
     static ARRAY_DEC(uint32_t, s_PolyMouseAssets, CURSOR_COUNT);
 #else
     static MouseThreadClass s_mouseThread;
     static bool s_mouseThreadIsDrawing;
     static HAnimClass *s_W3DMouseAssets1[CURSOR_COUNT];
     static RenderObjClass *s_W3DMouseAssets2[CURSOR_COUNT];
-    static TextureBaseClass *s_D3DMouseAssets[CURSOR_COUNT][21]; // TODO unsure on type
+    static TextureBaseClass *s_D3DMouseAssets[CURSOR_COUNT][MAX_FRAMES]; // TODO unsure on type
     static uint32_t s_PolyMouseAssets[CURSOR_COUNT];
 #endif
 };

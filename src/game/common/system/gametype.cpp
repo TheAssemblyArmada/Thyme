a2f838f2d43476aab4d99e98ef2853e8d1ec92e1
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2021-10-23 08:16:55 +0100

    Add trailing commas (as per style guide) to improve readability
    When implementing a couple of the parse functions I kept not seeing the first item on a list as it would be inline with the name confusing me as to if a list was identical. This makes it much clearer and follows the style guide

--- src/game/common/system/gametype.cpp
+++ src/game/common/system/gametype.cpp
@@ -14,34 +14,98 @@
  */
 #include "gametype.h"
 
-const char *g_timeOfDayNames[TIME_OF_DAY_COUNT + 1] = { "NONE", "MORNING", "AFTERNOON", "EVENING", "NIGHT", nullptr };
+const char *g_timeOfDayNames[TIME_OF_DAY_COUNT + 1] = {
+    "NONE",
+    "MORNING",
+    "AFTERNOON",
+    "EVENING",
+    "NIGHT",
+    nullptr,
+};
 
-const char *g_weatherNames[WEATHER_COUNT + 1]{ "NORMAL", "SNOWY", nullptr };
-const char *g_bodyDamageNames[BODY_COUNT + 1] = { "PRISTINE", "DAMAGED", "REALLYDAMAGED", "RUBBLE", nullptr };
+const char *g_weatherNames[WEATHER_COUNT + 1]{
+    "NORMAL",
+    "SNOWY",
+    nullptr,
+};
+const char *g_bodyDamageNames[BODY_COUNT + 1] = {
+    "PRISTINE",
+    "DAMAGED",
+    "REALLYDAMAGED",
+    "RUBBLE",
+    nullptr,
+};
 
 const char *g_speakerTypes[SPEAKERS_COUNT + 1] = {
-    "2 Speakers", "Headphones", "Surround Sound", "4 Speaker", "5.1 Surround", "7.1 Surround", nullptr
+    "2 Speakers",
+    "Headphones",
+    "Surround Sound",
+    "4 Speaker",
+    "5.1 Surround",
+    "7.1 Surround",
+    nullptr,
 };
 
-const char *g_audioPriorityNames[PRIORITY_COUNT + 1] = { "LOWEST", "LOW", "NORMAL", "HIGH", "CRITICAL", nullptr };
+const char *g_audioPriorityNames[PRIORITY_COUNT + 1] = {
+    "LOWEST",
+    "LOW",
+    "NORMAL",
+    "HIGH",
+    "CRITICAL",
+    nullptr,
+};
 
-const char *g_buildableStatusNames[BSTATUS_COUNT + 1] = { "Yes", "Ignore_Prerequisites", "No", "Only_By_AI", nullptr };
+const char *g_buildableStatusNames[BSTATUS_COUNT + 1] = {
+    "Yes",
+    "Ignore_Prerequisites",
+    "No",
+    "Only_By_AI",
+    nullptr,
+};
 
-const char *g_surfaceNames[SURFACE_COUNT + 1] = { "Ground", "Air", "Ground or Air", nullptr };
+const char *g_surfaceNames[SURFACE_COUNT + 1] = {
+    "Ground",
+    "Air",
+    "Ground or Air",
+    nullptr,
+};
 
 const char *g_shakeIntensityNames[SHAKE_COUNT + 1] = {
-    "Subtle", "Normal", "Strong", "Severe", "Cine_Extreme", "Cine_Insane", nullptr
+    "Subtle",
+    "Normal",
+    "Strong",
+    "Severe",
+    "Cine_Extreme",
+    "Cine_Insane",
+    nullptr,
 };
 
-const char *g_weaponSlotNames[WEAPONSLOT_COUNT + 1] = { "PRIMARY", "SECONDARY", "TERTIARY", nullptr };
+const char *g_weaponSlotNames[WEAPONSLOT_COUNT + 1] = {
+    "PRIMARY",
+    "SECONDARY",
+    "TERTIARY",
+    nullptr,
+};
 
 const char *g_commandSourceMaskNames[COMMANDSOURCE_COUNT + 1] = {
-    "FROM_PLAYER", "FROM_SCRIPT", "FROM_AI", "FROM_DOZER", "DEFAULT_SWITCH_WEAPON", nullptr
+    "FROM_PLAYER",
+    "FROM_SCRIPT",
+    "FROM_AI",
+    "FROM_DOZER",
+    "DEFAULT_SWITCH_WEAPON",
+    nullptr,
 };
 
-const char *g_veterancyNames[VETERANCY_COUNT + 1] = { "REGULAR", "VETERAN", "ELITE", "HEROIC", nullptr };
+const char *g_veterancyNames[VETERANCY_COUNT + 1] = {
+    "REGULAR",
+    "VETERAN",
+    "ELITE",
+    "HEROIC",
+    nullptr,
+};
 
-const char *g_particlePriorityNames[PARTICLE_PRIORITY_COUNT + 1] = { "NONE",
+const char *g_particlePriorityNames[PARTICLE_PRIORITY_COUNT + 1] = {
+    "NONE",
     "WEAPON_EXPLOSION",
     "SCORCHMARK",
     "DUST_TRAIL",
@@ -55,4 +119,5 @@ const char *g_particlePriorityNames[PARTICLE_PRIORITY_COUNT + 1] = { "NONE",
     "AREA_EFFECT",
     "CRITICAL",
     "ALWAYS_RENDER",
-    nullptr };
+    nullptr,
+};

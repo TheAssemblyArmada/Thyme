ce2eb3f274dcda2c231942b4e6fcac4f6263cfe1
xezon <4720891+xezon@users.noreply.github.com>
2021-10-31 16:27:03 +0100

    Improve hooking so that code page does not remain unprotected during entire process life time

--- src/hooker/dllmain.cpp
+++ src/hooker/dllmain.cpp
@@ -26,12 +26,14 @@ BOOL WINAPI DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved
 {
     switch (ul_reason_for_call) {
         case DLL_PROCESS_ATTACH:
-            StartHooking();
-            Setup_Hooks();
+            if (StartHooking()) {
+                Setup_Hooks();
+                StopHooking();
+            }
             break;
 
         case DLL_PROCESS_DETACH:
-            StopHooking();
+            // Currently does not unhook from module. Must shutdown.
             break;
 
         case DLL_THREAD_ATTACH:

c72a2519e7498de69d02fabc30818c749acdeb00
Duncan <duncans_pumpkin@hotmail.co.uk>
2021-11-08 17:11:32 +0000

    Implement ObjectCreationList and associated classes (#338)
    ObjectCreationList is not fully implemented.

---
+++ src/game/logic/object/objectcreationlist.cpp
@@ -0,0 +1,99 @@
+/**
+ * @file ObjectCreationList
+ *
+ * @author Duncans_pumpkin
+ *
+ * @brief ObjectCreationNugget, ObjectCreationList, ObjectCreationListStore
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+
+#include "objectcreationlist.h"
+#include "object.h"
+
+#ifndef GAME_DLL
+ObjectCreationListStore *g_theObjectCreationListStore = nullptr;
+#endif
+
+// zh: 0x004C1310 wb: 0x0075BB30
+ObjectCreationListStore::~ObjectCreationListStore()
+{
+    for (auto *nugget : m_nuggets) {
+        nugget->Delete_Instance();
+    }
+    Clear();
+}
+
+// zh: 0x004C1500 wb: 0x00756CFB
+ObjectCreationList *ObjectCreationListStore::Find_Object_Creation_List(const char *name)
+{
+    if (strcasecmp(name, "None") == 0) {
+        return nullptr;
+    }
+
+    const auto key = g_theNameKeyGenerator->Name_To_Key(name);
+
+    auto res = m_ocls.find(key);
+    if (res == m_ocls.end()) {
+        return nullptr;
+    }
+    return &res->second;
+}
+
+// wb: 0x00756D76
+void ObjectCreationListStore::Add_Object_Creation_Nugget(ObjectCreationNugget *nugget)
+{
+    m_nuggets.push_back(nugget);
+}
+
+// clang-format off
+static FieldParse s_theObjectCreationListFieldParse[] = {
+#ifdef GAME_DLL
+    { "CreateObject",       reinterpret_cast<inifieldparse_t>(PICK_ADDRESS(0x004BF350, 0x00759470)) /*&GenericObjectCreationNugget::Parse_Object */,    nullptr,  0 },
+    { "CreateDebris",       reinterpret_cast<inifieldparse_t>(PICK_ADDRESS(0x004BF730, 0x00759B60)) /*&GenericObjectCreationNugget::Parse_Debris */,    nullptr,  0 },
+    { "ApplyRandomForce",   reinterpret_cast<inifieldparse_t>(PICK_ADDRESS(0x004BEF70, 0x00758F20)) /*&ApplyRandomForceNugget::Parse */,                nullptr,  0 },
+    { "DeliverPayload",     reinterpret_cast<inifieldparse_t>(PICK_ADDRESS(0x004BDDD0, 0x00757920)) /*&DeliverPayloadNugget::Parse */,                  nullptr,  0 },
+    { "FireWeapon",         reinterpret_cast<inifieldparse_t>(PICK_ADDRESS(0x004BD7E0, 0x00756E00)) /*&FireWeaponNugget::Parse */,                      nullptr,  0 },
+    { "Attack",             reinterpret_cast<inifieldparse_t>(PICK_ADDRESS(0x004BD9A0, 0x007572C0)) /*&AttackNugget::Parse */,                          nullptr,  0 },
+#endif
+    { nullptr,              nullptr, nullptr,  0 },
+};
+// clang-format on
+
+// zh: 0x004C1570, wb: 0x00756D92
+void ObjectCreationListStore::Parse_Object_Creation_List_Definition(INI *ini)
+{
+    const auto key = g_theNameKeyGenerator->Name_To_Key(ini->Get_Next_Token());
+
+    auto &ocl = g_theObjectCreationListStore->m_ocls[key];
+    ocl.Clear();
+
+    ini->Init_From_INI(&ocl, s_theObjectCreationListFieldParse);
+}
+
+// zh: 0x004BD770 wb: 0x00756711
+void ObjectCreationNugget::Create(Object *primaryObj, const Object *secondaryObj, uint32_t unk)
+{
+    const Coord3D *secondary = nullptr;
+    if (secondaryObj != nullptr) {
+        secondary = secondaryObj->Get_Position();
+    }
+
+    const Coord3D *primary = nullptr;
+    if (primaryObj != nullptr) {
+        primary = primaryObj->Get_Position();
+    }
+
+    Create(primaryObj, primary, secondary, -100.0f, unk);
+}
+
+// zh: 0x004BD7B0 wb: 0x00756774
+void ObjectCreationNugget::Create(Object *primaryObj, const Coord3D *primary, const Coord3D *secondary, bool, uint32_t unk)
+{
+    Create(primaryObj, primary, secondary, -100.0f, unk);
+}
---
+++ src/game/logic/object/objectcreationlist.h
@@ -0,0 +1,81 @@
+/**
+ * @file ObjectCreationList
+ *
+ * @author Duncans_pumpkin
+ *
+ * @brief ObjectCreationNugget, ObjectCreationList, ObjectCreationListStore
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+
+#include "mempoolobj.h"
+#include "namekeygenerator.h"
+#include "subsysteminterface.h"
+#include <map>
+#include <vector>
+
+class INI;
+class Object;
+class Coord3D;
+
+class ObjectCreationNugget : public MemoryPoolObject
+{
+    IMPLEMENT_ABSTRACT_POOL(ObjectCreationNugget);
+
+public:
+    virtual ~ObjectCreationNugget() {}
+    virtual void Create(Object *primaryObj, const Coord3D *primary, const Coord3D *secondary, float unk1, uint32_t unk2) = 0;
+    virtual void Create(Object *primary, const Object *secondary, uint32_t unk);
+    virtual void Create(Object *primaryObj, const Coord3D *primary, const Coord3D *secondary, bool, uint32_t unk);
+};
+
+class ObjectCreationList
+{
+public:
+    ObjectCreationList(){};
+    ~ObjectCreationList(){};
+
+    void Clear() { m_nuggets.clear(); }
+    void Add_Object_Creation_Nugget(ObjectCreationNugget *nugget);
+    void Create(Object *primary, Object *secondary);
+    void Create(Object *primaryObj, Coord3D *primary, Coord3D *secondary);
+
+private:
+    std::vector<ObjectCreationNugget *> m_nuggets;
+};
+
+class ObjectCreationListStore : public SubsystemInterface
+{
+public:
+    ObjectCreationListStore(){};
+
+    virtual ~ObjectCreationListStore() override;
+    virtual void Init() override {}
+    virtual void Reset() override {}
+    virtual void Update() override {}
+
+    // zh: 0x004C1280 wb: 0x00756BBD
+    ObjectCreationListStore *Hook_Ctor() { return new (this) ObjectCreationListStore; }
+
+    ObjectCreationList *Find_Object_Creation_List(const char *name);
+    void Add_Object_Creation_Nugget(ObjectCreationNugget *nugget);
+
+    static void Parse_Object_Creation_List_Definition(INI *ini);
+
+private:
+    void Clear() { m_nuggets.clear(); }
+
+    std::map<NameKeyType, ObjectCreationList> m_ocls;
+    std::vector<ObjectCreationNugget *> m_nuggets;
+};
+
+#ifdef GAME_DLL
+extern ObjectCreationListStore *&g_theObjectCreationListStore;
+#else
+extern ObjectCreationListStore *g_theObjectCreationListStore;
+#endif

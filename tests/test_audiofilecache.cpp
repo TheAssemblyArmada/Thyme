5973cb069c4809dd29d80c28fa214e9280a402ee
Stephan Vedder <stephan.vedder@gmail.com>
2022-03-06 19:19:48 +0100

    Change nullptr

--- tests/test_audiofilecache.cpp
+++ tests/test_audiofilecache.cpp
@@ -34,11 +34,11 @@ TEST(audio, ffmpegaudiofilecache)
     auto filepath = Utf8String(TESTDATA_PATH) + "/audio/pcm1644m.wav";
     uint8_t *data = cache.Open_File(filepath);
     // We expect this to fail since our cache size is 0
-    EXPECT_EQ(data, nullptr);
+    EXPECT_EQ(data, NULL);
 
     // Use something that can hold our file
     cache.Set_Max_Size(0xFFFFF);
     data = cache.Open_File(filepath);
-    EXPECT_NE(data, nullptr);
+    EXPECT_NE(data, NULL);
 }
 #endif

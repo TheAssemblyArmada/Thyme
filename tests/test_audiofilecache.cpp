0642cb0f97e8a9bf8962f79ce300a9ed2ba00a8b
Stephan Vedder <vedder@mbits.info>
2022-03-07 09:28:42 +0100

    Fix test failure

--- tests/test_audiofilecache.cpp
+++ tests/test_audiofilecache.cpp
@@ -34,11 +34,11 @@ TEST(audio, ffmpegaudiofilecache)
     auto filepath = Utf8String(TESTDATA_PATH) + "/audio/pcm1644m.wav";
     uint8_t *data = cache.Open_File(filepath);
     // We expect this to fail since our cache size is 0
-    EXPECT_EQ(data, NULL);
+    EXPECT_TRUE(data == nullptr);
 
     // Use something that can hold our file
     cache.Set_Max_Size(0xFFFFF);
     data = cache.Open_File(filepath);
-    EXPECT_NE(data, NULL);
+    EXPECT_TRUE(data != nullptr);
 }
 #endif

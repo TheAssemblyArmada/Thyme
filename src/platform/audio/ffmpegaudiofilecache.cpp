52e79c8302c4cf56e99131ec72c299ed4f1e18d4
Stephan Vedder <stephan.vedder@gmail.com>
2022-03-09 10:09:27 +0100

    Fixes

--- src/platform/audio/ffmpegaudiofilecache.cpp
+++ src/platform/audio/ffmpegaudiofilecache.cpp
@@ -62,8 +62,8 @@ int FFmpegAudioFileCache::Read_FFmpeg_Packet(void *opaque, uint8_t *buf, int buf
 {
     File *file = static_cast<File *>(opaque);
     int read = file->Read(buf, buf_size);
-    if (read < 0)
-        return 0;
+    if (read <= 0)
+        return AVERROR_EOF;
 
     return read;
 }
@@ -74,7 +74,7 @@ int FFmpegAudioFileCache::Read_FFmpeg_Packet(void *opaque, uint8_t *buf, int buf
 bool FFmpegAudioFileCache::Open_FFmpeg_Contexts(FFmpegOpenAudioFile *open_audio, File *file)
 {
 #if LOGGING_LEVEL != LOGLEVEL_NONE
-    av_log_set_level(AV_LOG_TRACE);
+    av_log_set_level(AV_LOG_INFO);
 #endif
 
     av_register_all();
@@ -173,6 +173,7 @@ bool FFmpegAudioFileCache::Decode_FFmpeg(FFmpegOpenAudioFile *file)
     int result = 0;
     char error_buffer[1024];
 
+    // Read all packets inside the file
     while (av_read_frame(file->fmt_ctx, packet) >= 0) {
         result = avcodec_send_packet(file->codec_ctx, packet);
         if (result < 0) {
@@ -180,13 +181,18 @@ bool FFmpegAudioFileCache::Decode_FFmpeg(FFmpegOpenAudioFile *file)
             captainslog_error("Failed 'avcodec_send_packet': %s", error_buffer);
             return false;
         }
-        result = avcodec_receive_frame(file->codec_ctx, frame);
-        // Check if this was a real error or we just need more data
-        if (result < 0 && result != AVERROR(EAGAIN) && result != AVERROR_EOF) {
-            av_strerror(result, error_buffer, sizeof(error_buffer));
-            captainslog_error("Failed 'avcodec_receive_frame': %s", error_buffer);
-            return false;
-        } else if (result >= 0) {
+        // Decode all frames contained inside the packet
+        while (result >= 0) {
+            result = avcodec_receive_frame(file->codec_ctx, frame);
+            // Check if we need more data
+            if (result == AVERROR(EAGAIN) || result == AVERROR_EOF)
+                break;
+            else if (result < 0) {
+                av_strerror(result, error_buffer, sizeof(error_buffer));
+                captainslog_error("Failed 'avcodec_receive_frame': %s", error_buffer);
+                return false;
+            }
+
             int frame_data_size = av_samples_get_buffer_size(
                 NULL, file->codec_ctx->channels, frame->nb_samples, file->codec_ctx->sample_fmt, 1);
             file->wave_data = static_cast<uint8_t *>(av_realloc(file->wave_data, file->data_size + frame_data_size));

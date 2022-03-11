1bd8aa547295db6530309a6463460b77a564def0
Stephan Vedder <stephan.vedder@gmail.com>
2022-03-11 22:49:09 +0100

    Add CRC tests

---
+++ tests/test_crc.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file
+ *
+ * @author feliwir
+ *
+ * @brief Set of tests to validate the CRC32 implementation
+ *
+ * @copyright Thyme is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include <crc.h>
+#include <gtest/gtest.h>
+
+TEST(checksum, crc32)
+{
+    const char *data = "Hello world";
+    uint32_t data_crc = 0x8BD69E52;
+
+    EXPECT_EQ(CRC::Memory(data, strlen(data), 0), data_crc);
+    EXPECT_EQ(CRC::String(data, 0), data_crc);
+    EXPECT_NE(CRC::Stringi(data, 0), data_crc);
+
+    const char *data_upper = "HELLO WORLD";
+    uint32_t data_upper_crc = 0x87E5865B;
+
+    EXPECT_EQ(CRC::Memory(data_upper, strlen(data_upper), 0), data_upper_crc);
+    EXPECT_EQ(CRC::String(data_upper, 0), data_upper_crc);
+    EXPECT_EQ(CRC::Stringi(data_upper, 0), data_upper_crc);
+}

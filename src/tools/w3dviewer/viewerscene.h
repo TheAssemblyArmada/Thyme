a36d5a8f201de6720cdc9b4dee81fbca0acae928
Jonathan Wilson <jfwfreo@tpgi.com.au>
2022-04-01 20:16:42 +1000

    Implement ViewerSceneClass

--- src/tools/w3dviewer/viewerscene.h
+++ src/tools/w3dviewer/viewerscene.h
@@ -15,3 +15,43 @@
 #pragma once
 #include "w3dafx.h"
 #include "scene.h"
+
+class ViewerSceneIterator : public SceneIterator
+{
+public:
+    ViewerSceneIterator(RefMultiListClass<RenderObjClass> *list) :
+        m_robjIterator(list)
+    {
+    }
+    virtual ~ViewerSceneIterator() {}
+    virtual void First() { m_robjIterator.First(); }
+    virtual void Next() { m_robjIterator.Next(); };
+    virtual bool Is_Done() { return m_robjIterator.Is_Done(); };
+    virtual RenderObjClass *Current_Item() { return m_robjIterator.Peek_Obj(); };
+
+private:
+    RefMultiListIterator<RenderObjClass> m_robjIterator;
+};
+
+class ViewerSceneClass : public SimpleSceneClass
+{
+public:
+    ViewerSceneClass() : m_autoSwitchLod(false) {}
+    virtual ~ViewerSceneClass() override {}
+    virtual void Add_Render_Object(RenderObjClass *obj) override;
+    virtual void Customized_Render(RenderInfoClass &rinfo) override;
+    virtual void Visibility_Check(CameraClass *camera) override;
+    virtual void Set_Auto_Switch_LOD(bool set) { m_autoSwitchLod = set; }
+    virtual bool Get_Auto_Switch_LOD() { return m_autoSwitchLod; }
+    virtual void Add_LOD_Object(RenderObjClass *obj);
+    virtual void Remove_All_LOD_Objects();
+    virtual void Get_Bounding_Box(AABoxClass *box);
+    virtual void Get_Bounding_Sphere(SphereClass *sphere);
+    virtual SceneIterator *Get_LOD_Iterator();
+    virtual void Destroy_LOD_Iterator(SceneIterator *iterator);
+
+private:
+    bool m_autoSwitchLod;
+    RefMultiListClass<RenderObjClass> m_lodObjectList;
+    RefMultiListClass<RenderObjClass> m_lightList;
+};

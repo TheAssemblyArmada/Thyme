6f091d19d56231a8cc6c5ce974c608ead5b7942c
Stephan Vedder <stephan.vedder@gmail.com>
2023-02-12 22:48:09 +0100

    Implement requested changes

--- src/tools/compressor/compressor.cpp
+++ src/tools/compressor/compressor.cpp
@@ -1,5 +1,6 @@
 #include "bufffileclass.h"
 #include "compressionmanager.h"
+#include "rtsutils.h"
 #include <cxxopts.hpp>
 
 int main(int argc, char **argv)
@@ -14,13 +15,13 @@ int main(int argc, char **argv)
     ("i,input", "input file", cxxopts::value<std::string>())
     ("o,output", "output file", cxxopts::value<std::string>())
     ("d,decompress", "decompress the input")
-    ("t,type", "specify the encoding format (encode only)", cxxopts::value<std::string>())
+    ("t,type", "specify the encoding format (EAR, ZL1-ZL9)", cxxopts::value<std::string>())
     ("h,help", "print usage")
     ("v,verbose", "verbose output", cxxopts::value<bool>()->default_value("false"))
     ;
     // clang-format on
 
-    auto result = options.parse(argc, argv);
+    const auto result = options.parse(argc, argv);
     if (result.count("help")) {
         std::cout << options.help() << std::endl;
         return EXIT_SUCCESS;
@@ -38,24 +39,24 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    auto input_path = result["input"].as<std::string>();
+    const auto input_path = result["input"].as<std::string>();
     BufferedFileClass input_file(input_path.c_str());
     if (!input_file.Open(FM_READ)) {
         std::cerr << "Failed to open input file:" << input_path << std::endl;
         return EXIT_FAILURE;
     }
 
-    auto output_path = result["output"].as<std::string>();
+    const auto output_path = result["output"].as<std::string>();
     BufferedFileClass output_file(output_path.c_str());
     if (!output_file.Open(FM_WRITE)) {
         std::cerr << "Failed to open output file:" << output_path << std::endl;
         return EXIT_FAILURE;
     }
 
-    auto input_size = input_file.Size();
+    const auto input_size = input_file.Size();
     std::unique_ptr<uint8_t[]> input_data(new uint8_t[input_size]);
     if (input_file.Read(input_data.get(), input_size) != input_size) {
-        std::cerr << "Failed to read input data completly" << std::endl;
+        std::cerr << "Failed to read input data completely" << std::endl;
         return EXIT_FAILURE;
     }
 
@@ -66,8 +67,12 @@ int main(int argc, char **argv)
             return EXIT_FAILURE;
         }
 
-        auto output_size = CompressionManager::Get_Uncompressed_Size(input_data.get(), input_size);
-        std::unique_ptr<uint8_t[]> output_data(new uint8_t[output_size]);
+        const auto output_size = CompressionManager::Get_Uncompressed_Size(input_data.get(), input_size);
+        if (output_size == 0) {
+            std::cerr << "The output size would be 0. Not supported" << std::endl;
+            return EXIT_FAILURE;
+        }
+        const std::unique_ptr<uint8_t[]> output_data(new uint8_t[output_size]);
 
         if (CompressionManager::Decompress_Data(input_data.get(), input_size, output_data.get(), output_size)
             != output_size) {
@@ -76,14 +81,15 @@ int main(int argc, char **argv)
         }
 
         if (output_file.Write(output_data.get(), output_size) != output_size) {
-            std::cerr << "Failed to write output data completly" << std::endl;
+            std::cerr << "Failed to write output data completely" << std::endl;
             return EXIT_FAILURE;
         }
     } else {
         CompressionType type = CompressionManager::Get_Prefered_Compression();
         if (result.count("type") > 0) {
-            auto compr_name = result["type"].as<std::string>();
-            type = CompressionManager::Get_Compression_Type(compr_name.c_str(), compr_name.size() + 5);
+            const auto compr_name = result["type"].as<std::string>();
+            const uint32_t fourCC = rts::FourCC_From_String(compr_name.c_str());
+            type = CompressionManager::Get_Compression_Type_By_FourCC(fourCC);
 
             if (type == COMPRESSION_NONE) {
                 std::cerr << "Unknown compression type: " << compr_name << std::endl;
@@ -92,7 +98,7 @@ int main(int argc, char **argv)
         }
 
         auto output_size = CompressionManager::Get_Max_Compressed_Size(input_size, type);
-        std::unique_ptr<uint8_t[]> output_data(new uint8_t[output_size]);
+        const std::unique_ptr<uint8_t[]> output_data(new uint8_t[output_size]);
 
         output_size = CompressionManager::Compress_Data(type, input_data.get(), input_size, output_data.get(), output_size);
         if (output_size == 0) {
@@ -101,7 +107,7 @@ int main(int argc, char **argv)
         }
 
         if (output_file.Write(output_data.get(), output_size) != output_size) {
-            std::cerr << "Failed to write output data completly" << std::endl;
+            std::cerr << "Failed to write output data completely" << std::endl;
             return EXIT_FAILURE;
         }
     }

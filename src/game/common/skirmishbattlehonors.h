54999d2cf785fa43e8a9a290c246bf8e938ccf1b
psykana <36602558+psykana@users.noreply.github.com>
2024-03-27 22:55:09 +0000

    Implement some SkirmishBattleHonors (#1126)

--- src/game/common/skirmishbattlehonors.h
+++ src/game/common/skirmishbattlehonors.h
@@ -14,6 +14,7 @@
  */
 #pragma once
 #include "always.h"
+#include "gamewindow.h"
 #include "userpreferences.h"
 
 class SkirmishBattleHonors : public UserPreferences
@@ -22,4 +23,25 @@ public:
     SkirmishBattleHonors();
     virtual ~SkirmishBattleHonors() override;
     int Get_Endurance_Medal(Utf8String name, int level) const;
+    int Get_Wins() const;
+    int Get_Win_Streak() const;
+    int Get_Best_Win_Streak() const;
+    int Get_Losses() const;
+    int Get_China_Campaign_Complete(int level) const;
+    int Get_USA_Campaign_Complete(int level) const;
+    int Get_GLA_Campaign_Complete(int level) const;
+    int Get_Challenge_Campaign_Complete(int general, int level) const;
+    int Get_Honors() const;
 };
+
+void Reset_Battle_Honor_Insertion();
+void Insert_Battle_Honor(
+    GameWindow *window, Image const *image, bool earned, int tooltip, int *row, int *column, Utf16String str, int score);
+void Battle_Honor_Tooltip(GameWindow *window, WinInstanceData *instance, unsigned int mouse);
+void Populate_Skirmish_Battle_Honors();
+
+#ifdef GAME_DLL
+extern int &g_rowsToSkip;
+#else
+extern int g_rowsToSkip;
+#endif
